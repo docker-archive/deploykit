ef00554bf6b9afe5b51dd856ff40258dfe150c7a
David Chung <david.chung@docker.com>
2016-04-27 12:56:49 -0700

    Struct field checker

---
+++ check.go
@@ -0,0 +1,158 @@
+package libmachete
+
+import (
+	"fmt"
+	"reflect"
+	"sort"
+	"strings"
+)
+
+// TagGetter is a function that the callback can use to access other tags in the field.
+// For example, the callback may want to access the `json` tag in the field to report to
+// the user the field name as defined in the tag.
+type TagGetter func(tag string) string
+
+// FieldCheckCallback callbacks are called when a struct field fails validation.  Return true to stop/error immediately
+type FieldCheckCallback func(value interface{}, fieldName string, getter TagGetter) bool
+
+// CheckFields checks the fields in the given struct to see if any of the fields are
+// nil or zero value when the field annotation / tag indicates otherwise.  The client
+// can use callback to be notified of the event or pass nil for the callbacks and instead
+// check the error which contains lists of field names in violation.  The callbacks can return
+// false to stop the processing.
+func CheckFields(v interface{}, onMissing, onZero FieldCheckCallback) error {
+	if reflect.TypeOf(v).Kind() == reflect.Ptr && reflect.TypeOf(v).Elem().Kind() == reflect.Struct {
+		err := new(ErrStructFields)
+		callbacks := map[string]FieldCheckCallback{
+			ruleNotNil:  ensureNotNil(onMissing),
+			ruleNotZero: ensureNotNil(onZero),
+		}
+		checkFields(v, callbacks, err)
+		if len(err.Names) > 0 {
+			return err
+		}
+		return nil
+	}
+	return fmt.Errorf("Target must be a pointer to struct. Not %v", reflect.TypeOf(v))
+}
+
+// ErrStructFields is error used to collect the names of fields that violate the checks.
+type ErrStructFields struct {
+	Names []string
+}
+
+func (e *ErrStructFields) append(v string) {
+	e.Names = append(e.Names, v)
+}
+
+// SortNames returns a sorted slice of names
+func (e *ErrStructFields) SortNames() []string {
+	sort.Strings(e.Names)
+	return e.Names
+}
+
+func (e *ErrStructFields) Error() string {
+	return fmt.Sprintf("Invalid entry: missing or zero values in fields:%s",
+		strings.Join(e.Names, ","))
+}
+
+func ensureNotNil(f FieldCheckCallback) FieldCheckCallback {
+	if f == nil {
+		return func(interface{}, string, TagGetter) bool {
+			return false
+		}
+	}
+	return f
+}
+
+const (
+	ruleNotNil  = "not_nil"
+	ruleNotZero = "not_zero"
+)
+
+var (
+	// The functions here returns TRUE on failure / violation
+	fieldChecks = map[string]func(reflect.Value) bool{
+		ruleNotNil:  violateNotNil,
+		ruleNotZero: violateNotZero,
+	}
+)
+
+// Returns true if the value violates the not nil rule.
+func violateNotNil(v reflect.Value) bool {
+	if v.Type().Kind() == reflect.Ptr {
+		return v.IsNil()
+	}
+	return false
+}
+
+// Returns true if the value violates the not zero rule
+func violateNotZero(v reflect.Value) bool {
+	v = reflect.Indirect(v) // if v is pointer, indirect returns *v, otherwise just v
+	if v.IsValid() {
+		zero := reflect.Zero(v.Type())
+		return reflect.DeepEqual(v.Interface(), zero.Interface())
+	}
+	return false
+}
+
+// val should be a pointer
+func checkFields(val interface{}, callbacks map[string]FieldCheckCallback, err *ErrStructFields) bool {
+	t := reflect.TypeOf(val).Elem()
+	v := reflect.Indirect(reflect.ValueOf(val))
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		fieldValue := v.Field(i)
+
+		// See https://golang.org/ref/spec#Uniqueness_of_identifiers
+		exported := field.PkgPath == ""
+		if !exported {
+			continue
+		}
+
+		switch {
+		// Embedded struct
+		case field.Anonymous && field.Type.Kind() == reflect.Struct:
+			// A struct has been embedded in this struct.
+			if checkFields(fieldValue.Addr().Interface(), callbacks, err) {
+				return true
+			}
+			continue
+		case field.Type.Kind() == reflect.Struct:
+			if checkFields(fieldValue.Addr().Interface(), callbacks, err) {
+				return true
+			}
+			continue
+		case field.Type.Kind() == reflect.Ptr && field.Type.Elem().Kind() == reflect.Struct:
+			if checkFields(fieldValue.Interface(), callbacks, err) {
+				return true
+			}
+			continue
+		}
+
+		// Normal fields
+		tag := field.Tag
+		spec := tag.Get("check")
+		if spec == "" {
+			continue
+		}
+
+		rules := strings.Split(spec, ",")
+		for _, name := range rules {
+			rule, has := fieldChecks[name]
+			if !has {
+				panic(fmt.Errorf("Programming error: bad rule:", name))
+			}
+			if rule(fieldValue) {
+				err.append(field.Name)
+				if cb, has := callbacks[name]; has {
+					if cb(val, field.Name, func(t string) string { return tag.Get(t) }) {
+						return true // true to stop
+					}
+				}
+			}
+		}
+	}
+	return false
+}
---
+++ check_test.go
@@ -0,0 +1,254 @@
+package libmachete
+
+import (
+	"github.com/stretchr/testify/require"
+	"reflect"
+	"sort"
+	"testing"
+)
+
+func TestChecks(t *testing.T) {
+
+	theName := "name"
+	theInt := 10
+
+	v := &struct {
+		Name     string
+		NamePtr  *string
+		Int      int
+		IntPtr   *int
+		ZeroName string
+		NilName  *string
+		ZeroInt  int
+		NilInt   *int
+	}{
+		Name:    theName,
+		NamePtr: &theName,
+		Int:     theInt,
+		IntPtr:  &theInt,
+	}
+
+	// Since we provide values for the Name, NamePtr, Int, and IntPtr fields, none of these
+	// should violate the not nil or not zero rules.
+	require.False(t, violateNotNil(reflect.ValueOf(v.Name)))
+	require.False(t, violateNotNil(reflect.ValueOf(v.NamePtr)))
+	require.False(t, violateNotNil(reflect.ValueOf(v.Int)))
+	require.False(t, violateNotNil(reflect.ValueOf(v.IntPtr)))
+
+	require.False(t, violateNotZero(reflect.ValueOf(v.Name)))
+	require.False(t, violateNotZero(reflect.ValueOf(v.NamePtr)))
+	require.False(t, violateNotZero(reflect.ValueOf(v.Int)))
+	require.False(t, violateNotZero(reflect.ValueOf(v.IntPtr)))
+
+	// For the fields that are not set.  We check to see if they violate the not nil/ zero checks.
+	require.False(t, violateNotNil(reflect.ValueOf(v.ZeroName)))
+	require.True(t, violateNotNil(reflect.ValueOf(v.NilName)))
+	require.False(t, violateNotNil(reflect.ValueOf(v.ZeroInt)))
+	require.True(t, violateNotNil(reflect.ValueOf(v.NilInt)))
+
+	require.True(t, violateNotZero(reflect.ValueOf(v.ZeroName)))
+	require.False(t, violateNotZero(reflect.ValueOf(v.NilName)))
+	require.True(t, violateNotZero(reflect.ValueOf(v.ZeroInt)))
+	require.False(t, violateNotZero(reflect.ValueOf(v.NilInt)))
+}
+
+type input struct {
+	String string `label:"the_string" check:"not_zero"`
+	Int    int    `label:"the_int" check:"not_zero"`
+	Bool   bool   `label:"the_bool" check:"not_zero"`
+
+	StringPtr *string `label:"string_ptr" check:"not_nil,not_zero"`
+	IntPtr    *int    `label:"int_ptr" check:"not_nil"`
+	BoolPtr   *bool   `label:"bool_ptr" check:"not_nil"`
+
+	DontCareString string
+	DontCareInt    int
+	DontCareBool   bool
+
+	DontCareStringPtr *string
+	DontCareIntPtr    *int
+	DontCareBoolPTr   *bool
+}
+
+func TestCheckFields(t *testing.T) {
+
+	theString := "string"
+	theInt := 100
+	theBool := true
+
+	// Case - when everything required is provided
+	target := &input{
+		String: theString,
+		Int:    theInt,
+		Bool:   theBool,
+
+		StringPtr: &theString,
+		IntPtr:    &theInt,
+		BoolPtr:   &theBool,
+	}
+	err := CheckFields(target, nil, nil)
+	require.Nil(t, err)
+
+	// Case - when required pointer fields are missing
+	target = &input{
+		String: theString,
+		Int:    theInt,
+		Bool:   theBool,
+	}
+	err = CheckFields(target, nil, nil)
+	require.NotNil(t, err)
+	require.Equal(t, []string{"BoolPtr", "IntPtr", "StringPtr"}, err.(*ErrStructFields).SortNames())
+
+	// Case -- when required value fields are missing
+	target = &input{
+		StringPtr: &theString,
+		IntPtr:    &theInt,
+		BoolPtr:   &theBool,
+	}
+	err = CheckFields(target, nil, nil)
+	require.NotNil(t, err)
+	require.Equal(t, []string{"Bool", "Int", "String"}, err.(*ErrStructFields).SortNames())
+
+	// Case - when required pointer field is provided but is empty string
+	emptyString := ""
+	zeroInt := 0
+	zeroBool := false
+	target = &input{
+		StringPtr: &emptyString,
+		IntPtr:    &zeroInt,  // zero is allowed (no 'not_zero')
+		BoolPtr:   &zeroBool, // zero is allowed (no 'not_zero')
+	}
+	err = CheckFields(target, nil, nil)
+	require.NotNil(t, err)
+	require.Equal(t, []string{"Bool", "Int", "String", "StringPtr"}, err.(*ErrStructFields).SortNames())
+}
+
+func TestCheckFieldCallbacks(t *testing.T) {
+
+	// Case - when required pointer field is provided but is empty string
+	emptyString := ""
+	zeroInt := 0
+	zeroBool := false
+	target := &input{
+		StringPtr: &emptyString,
+		IntPtr:    &zeroInt,  // zero is allowed (no 'not_zero')
+		BoolPtr:   &zeroBool, // zero is allowed (no 'not_zero')
+	}
+
+	missingPtr := new(int)
+	zeros := new(int)
+	zeroLabels := []string{}
+
+	err := CheckFields(target,
+		func(v interface{}, n string, g TagGetter) bool {
+			*missingPtr++
+			return false
+		},
+		func(v interface{}, n string, g TagGetter) bool {
+			*zeros++
+
+			// Get the label of the field
+			zeroLabels = append(zeroLabels, g("label"))
+			return false
+		})
+
+	require.NotNil(t, err)
+	require.Equal(t, []string{"Bool", "Int", "String", "StringPtr"}, err.(*ErrStructFields).SortNames())
+	require.Equal(t, 0, *missingPtr) // We provided a pointer but it points to an empty string, so no missing pointers
+	require.Equal(t, 4, *zeros)
+
+	// Test / demo on how to access other field tags --> this is useful for reporting missing yaml or json fields.
+	sort.Strings(zeroLabels)
+	require.Equal(t, []string{"string_ptr", "the_bool", "the_int", "the_string"}, zeroLabels)
+
+	// Case - callback returns immediately on the first error
+	*missingPtr = 0
+	*zeros = 0
+	err = CheckFields(target,
+		func(v interface{}, n string, g TagGetter) bool {
+			*missingPtr++
+			return true
+		},
+		func(v interface{}, n string, g TagGetter) bool {
+			*zeros++
+			return true
+		})
+
+	require.NotNil(t, err)
+	// String is the first declared field.
+	require.Equal(t, []string{"String"}, err.(*ErrStructFields).SortNames())
+	require.Equal(t, 0, *missingPtr)
+	require.Equal(t, 1, *zeros)
+
+}
+
+type sub struct {
+	StringPtr *string `check:"not_nil,not_zero"`
+	IntPtr    *int    `check:"not_nil"`
+	BoolPtr   *bool   `check:"not_nil"`
+}
+
+type nested struct {
+	String string `check:"not_zero"`
+	Int    int    `check:"not_zero"`
+	Bool   bool   `check:"not_zero"`
+	Nested sub
+}
+
+func TestCheckFieldsNested(t *testing.T) {
+
+	theString := "string"
+	theInt := 100
+	theBool := true
+
+	// Case - when everything required is provided
+	target := &nested{
+		String: theString,
+		Int:    theInt,
+		Bool:   theBool,
+		Nested: sub{
+			StringPtr: &theString,
+			IntPtr:    &theInt,
+			BoolPtr:   &theBool,
+		},
+	}
+	err := CheckFields(target, nil, nil)
+	require.Nil(t, err)
+
+	// Case - when required pointer fields are missing
+	target = &nested{
+		String: theString,
+		Int:    theInt,
+		Bool:   theBool,
+	}
+	err = CheckFields(target, nil, nil)
+	require.NotNil(t, err)
+	require.Equal(t, []string{"BoolPtr", "IntPtr", "StringPtr"}, err.(*ErrStructFields).SortNames())
+
+	// Case -- when required value fields are missing
+	target = &nested{
+		Nested: sub{
+			StringPtr: &theString,
+			IntPtr:    &theInt,
+			BoolPtr:   &theBool,
+		},
+	}
+	err = CheckFields(target, nil, nil)
+	require.NotNil(t, err)
+	require.Equal(t, []string{"Bool", "Int", "String"}, err.(*ErrStructFields).SortNames())
+
+	// Case - when required pointer field is provided but is empty string
+	emptyString := ""
+	zeroInt := 0
+	zeroBool := false
+	target = &nested{
+		Nested: sub{
+			StringPtr: &emptyString,
+			IntPtr:    &zeroInt,  // zero is allowed (no 'not_zero')
+			BoolPtr:   &zeroBool, // zero is allowed (no 'not_zero')
+		},
+	}
+	err = CheckFields(target, nil, nil)
+	require.NotNil(t, err)
+	require.Equal(t, []string{"Bool", "Int", "String", "StringPtr"}, err.(*ErrStructFields).SortNames())
+}
