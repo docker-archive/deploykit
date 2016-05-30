b306a0df9ec8917b108f7c1c755993f3c2a4ed2b
Bill Farner <wfarner@apache.org>
2016-05-30 11:02:15 -0700

    Add test for template HTTP error conditions.

--- cmd/machined/http/templates_test.go
+++ cmd/machined/http/templates_test.go
@@ -2,40 +2,13 @@ package http
 
 import (
 	"encoding/json"
-	"github.com/docker/libmachete"
-	mock_api "github.com/docker/libmachete/mock/provisioners/api"
 	"github.com/docker/libmachete/provisioners/api"
 	"github.com/docker/libmachete/storage"
-	"github.com/docker/libmachete/storage/filestores"
 	"github.com/drewolson/testflight"
 	"github.com/golang/mock/gomock"
-	"github.com/spf13/afero"
 	"github.com/stretchr/testify/require"
-	"net/http"
 	"testing"
 )
-
-func prepareTest(t *testing.T, ctrl *gomock.Controller) (*mock_api.MockProvisioner, http.Handler) {
-	provisioner := mock_api.NewMockProvisioner(ctrl)
-
-	builder := libmachete.ProvisionerBuilder{
-		Name:                  "testcloud",
-		DefaultCredential:     nil,
-		DefaultMachineRequest: func() api.MachineRequest { return &api.BaseMachineRequest{} },
-		Build: func(controls api.ProvisionControls, cred api.Credential) (api.Provisioner, error) {
-			return provisioner, nil
-		},
-	}
-
-	sandbox := filestores.NewSandbox(afero.NewMemMapFs(), "/")
-
-	server, err := build(sandbox, libmachete.NewMachineProvisioners([]libmachete.ProvisionerBuilder{builder}))
-	require.NoError(t, err)
-
-	return provisioner, server.getHandler()
-}
-
-const JSON = "application/json"
 
 func marshalTemplate(t *testing.T, template api.BaseMachineRequest) string {
 	body, err := json.Marshal(template)
@@ -50,13 +23,13 @@ func unmarshalTemplate(t *testing.T, data string) api.BaseMachineRequest {
 	return value
 }
 
-type ListResponse []storage.TemplateID
+type TemplateList []storage.TemplateID
 
-func requireListResult(t *testing.T, r *testflight.Requester, expected ListResponse) {
+func requireTemplates(t *testing.T, r *testflight.Requester, expected TemplateList) {
 	response := r.Get("/templates/json")
 	require.Equal(t, 200, response.StatusCode)
 	require.Equal(t, JSON, response.Header.Get("Content-Type"))
-	payload := ListResponse{}
+	payload := TemplateList{}
 	require.NoError(t, json.Unmarshal([]byte(response.Body), &payload))
 	require.Equal(t, expected, payload)
 }
@@ -69,7 +42,7 @@ func TestTemplateCrud(t *testing.T) {
 
 	testflight.WithServer(handler, func(r *testflight.Requester) {
 		// There should initially be no templates
-		requireListResult(t, r, ListResponse{})
+		requireTemplates(t, r, TemplateList{})
 
 		// Create a template
 		template := api.BaseMachineRequest{MachineName: "test", Provisioner: "testcloud"}
@@ -84,7 +57,7 @@ func TestTemplateCrud(t *testing.T) {
 		id := storage.TemplateID{Provisioner: "testcloud", Name: "prodtemplate"}
 
 		// It should appear in a list request
-		requireListResult(t, r, ListResponse{id})
+		requireTemplates(t, r, TemplateList{id})
 
 		// Update the template
 		updated := api.BaseMachineRequest{MachineName: "testnew", Provisioner: "testcloud"}
@@ -97,13 +70,43 @@ func TestTemplateCrud(t *testing.T) {
 		require.Equal(t, updated, unmarshalTemplate(t, response.Body))
 
 		// It should still appear in a list request
-		requireListResult(t, r, ListResponse{id})
+		requireTemplates(t, r, TemplateList{id})
 
 		// Delete the template
 		require.Equal(t, 200, r.Delete("/templates/testcloud/prodtemplate", JSON, "").StatusCode)
 
 		// It should no longer exist
 		require.Equal(t, 404, r.Get("/templates/testcloud/prodtemplate/json").StatusCode)
-		requireListResult(t, r, ListResponse{})
+		requireTemplates(t, r, TemplateList{})
+	})
+}
+
+func TestErrorResponses(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	_, handler := prepareTest(t, ctrl)
+
+	testflight.WithServer(handler, func(r *testflight.Requester) {
+		template := api.BaseMachineRequest{MachineName: "test", Provisioner: "testcloud"}
+
+		// Non-existent provisioner and/or template.
+		require.Equal(t, 400, r.Get("/templates/meta/absentprovisioner/json").StatusCode)
+		require.Equal(t, 404, r.Post(
+			"/templates/absentprovisioner/name/create",
+			JSON,
+			marshalTemplate(t, template)).StatusCode)
+		require.Equal(t, 404, r.Put(
+			"/templates/absentprovisioner/name",
+			JSON,
+			marshalTemplate(t, template)).StatusCode)
+		require.Equal(t, 404, r.Get("/templates/absentprovisioner/name").StatusCode)
+		require.Equal(t, 404, r.Delete("/templates/absentprovisioner/name", JSON, "").StatusCode)
+
+		// Duplicate template.
+		response := r.Post("/templates/testcloud/prodtemplate/create", JSON, marshalTemplate(t, template))
+		require.Equal(t, 200, response.StatusCode)
+		response = r.Post("/templates/testcloud/prodtemplate/create", JSON, marshalTemplate(t, template))
+		require.Equal(t, 409, response.StatusCode)
 	})
 }
