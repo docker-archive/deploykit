2cfdfa00da30d3e1c6d32b2239496429c7a98754
Bill Farner <bill@docker.com>
2016-07-20 11:12:15 -0700

    Eliminate intermediate struct for defining AWS instances

--- provider/aws/instance_test.go
+++ provider/aws/instance_test.go
@@ -1,7 +1,6 @@
 package aws
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
@@ -26,11 +25,7 @@ func TestCreateInstanceSuccess(t *testing.T) {
 	instanceID := "test-id"
 	reservation := ec2.Reservation{Instances: []*ec2.Instance{{InstanceId: &instanceID}}}
 
-	expectedRunInput := ec2.RunInstancesInput{}
-	err := json.Unmarshal([]byte(outputJSON), &expectedRunInput)
-	require.NoError(t, err)
-
-	clientMock.EXPECT().RunInstances(&expectedRunInput).Return(&reservation, nil)
+	clientMock.EXPECT().RunInstances(gomock.Any()).Return(&reservation, nil)
 
 	tagRequest := ec2.CreateTagsInput{
 		Resources: []*string{&instanceID},
@@ -192,96 +187,40 @@ const (
 	inputJSON = `
 {
     "group": "group",
-    "availability_zone": "us-west-2a",
-    "image_id": "ami-30ee0d50",
-    "block_device_name": "/dev/sdb",
-    "root_size": 64,
-    "volume_type": "gp2",
-    "delete_on_termination": true,
-    "security_group_ids": [
-      "sg-973491f0"
-    ],
-    "subnet_id": "",
-    "instance_type": "t2.micro",
-    "private_ip_ddress": "",
-    "associate_public_ip_address": true,
-    "private_ip_only": false,
-    "ebs_optimized": false,
-    "iam_instance_profile": "",
-    "tags": {
-      "test": "aws-create-test"
-    },
-    "key_name": "dev",
-    "vpc_id": "vpc-74c22510",
-    "monitoring": true
-}
-`
-
-	outputJSON = `
-{
-    "AdditionalInfo": null,
-    "BlockDeviceMappings": [
-      {
-        "DeviceName": "/dev/sdb",
-        "Ebs": {
-          "DeleteOnTermination": true,
-          "Encrypted": null,
-          "Iops": null,
-          "SnapshotId": null,
-          "VolumeSize": 64,
-          "VolumeType": "gp2"
-        },
-        "NoDevice": null,
-        "VirtualName": null
-      }
-    ],
-    "ClientToken": null,
-    "DisableApiTermination": null,
-    "DryRun": null,
-    "EbsOptimized": false,
-    "IamInstanceProfile": {
-      "Arn": null,
-      "Name": ""
-    },
-    "ImageId": "ami-30ee0d50",
-    "InstanceInitiatedShutdownBehavior": null,
-    "InstanceType": "t2.micro",
-    "KernelId": null,
-    "KeyName": "dev",
-    "MaxCount": 1,
-    "MinCount": 1,
-    "Monitoring": {
-      "Enabled": true
-    },
-    "NetworkInterfaces": [
-      {
-        "AssociatePublicIpAddress": true,
-        "DeleteOnTermination": true,
-        "Description": null,
-        "DeviceIndex": 0,
-        "Groups": [
-          "sg-973491f0"
+    "tags": {"test": "aws-create-test"},
+    "run_instances_input": {
+        "BlockDeviceMappings": [
+          {
+            "DeviceName": "/dev/sdb",
+            "Ebs": {
+                "DeleteOnTermination": true,
+                "VolumeSize": 64,
+                "VolumeType": "gp2"
+            }
+          }
         ],
-        "NetworkInterfaceId": null,
-        "PrivateIpAddress": null,
-        "PrivateIpAddresses": null,
-        "SecondaryPrivateIpAddressCount": null,
-        "SubnetId": ""
-      }
-    ],
-    "Placement": {
-      "Affinity": null,
-      "AvailabilityZone": "us-west-2a",
-      "GroupName": null,
-      "HostId": null,
-      "Tenancy": null
-    },
-    "PrivateIpAddress": null,
-    "RamdiskId": null,
-    "SecurityGroupIds": null,
-    "SecurityGroups": null,
-    "SubnetId": null,
-    "UserData": null
-  }
+        "EbsOptimized": false,
+        "ImageId": "ami-30ee0d50",
+        "InstanceType": "t2.micro",
+        "KeyName": "dev",
+        "Monitoring": {
+            "Enabled": true
+        },
+        "NetworkInterfaces": [
+          {
+            "AssociatePublicIpAddress": true,
+            "DeleteOnTermination": true,
+            "DeviceIndex": 0,
+            "Groups": [
+                "sg-973491f0"
+            ],
+            "SubnetId": "subnet-2"
+          }
+        ],
+        "Placement": {
+            "AvailabilityZone": "us-west-2a"
+        }
+    }
+}
 `
 )
