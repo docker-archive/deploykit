c660e0d8d0b08942091c2610a73a399c8cefd9b3
David Chung <david.chung@docker.com>
2016-07-24 20:33:27 -0700

    Container for loadbalancer controller

---
+++ controller/loadbalancer/cmd/elb.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"github.com/docker/libmachete/provider/aws"
+	"github.com/docker/libmachete/spi/loadbalancer"
+	"github.com/spf13/cobra"
+)
+
+var elbOptions = new(aws.Options)
+
+func elbCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "elb",
+		Short: "Ops on the ELB",
+	}
+	cmd.PersistentFlags().IntVar(&elbOptions.Retries,
+		"retries", 10, "Retries")
+	cmd.PersistentFlags().StringVar(&elbOptions.Region,
+		"region", "", "Region")
+
+	describeCmd := &cobra.Command{
+		Use:   "describe",
+		Short: "Describes the ELB",
+		RunE: func(_ *cobra.Command, args []string) error {
+			log.Infoln("Running describe ELB")
+
+			name := args[0]
+
+			p, err := aws.NewLoadBalancerDriver(aws.CreateELBClient(aws.Credentials(nil), *elbOptions), name)
+			if err != nil {
+				return err
+			}
+			result, err := p.State()
+			if err != nil {
+				return err
+			}
+			fmt.Println(result)
+			return nil
+		},
+	}
+
+	registerCmd := &cobra.Command{
+		Use:   "register",
+		Short: "Register an instance",
+		RunE: func(_ *cobra.Command, args []string) error {
+			log.Infoln("Running register instance for ELB")
+
+			name := args[0]
+			instanceID := args[1]
+
+			p, err := aws.NewLoadBalancerDriver(aws.CreateELBClient(aws.Credentials(nil), *elbOptions), name)
+			if err != nil {
+				return err
+			}
+			result, err := p.RegisterBackend(instanceID)
+			if err != nil {
+				return err
+			}
+			fmt.Println(result)
+			return nil
+		},
+	}
+
+	deregisterCmd := &cobra.Command{
+		Use:   "deregister",
+		Short: "De-register an instance",
+		RunE: func(_ *cobra.Command, args []string) error {
+			log.Infoln("Running register instance for ELB")
+
+			name := args[0]
+			instanceID := args[1]
+
+			p, err := aws.NewLoadBalancerDriver(aws.CreateELBClient(aws.Credentials(nil), *elbOptions), name)
+			if err != nil {
+				return err
+			}
+			result, err := p.DeregisterBackend(instanceID)
+			if err != nil {
+				return err
+			}
+			fmt.Println(result)
+			return nil
+		},
+	}
+
+	publishOptions := new(struct {
+		ExtPort     uint32
+		BackendPort uint32
+		Protocol    string
+	})
+
+	publishCmd := &cobra.Command{
+		Use:   "publish",
+		Short: "Publish a service at given ports",
+		RunE: func(_ *cobra.Command, args []string) error {
+			name := args[0]
+
+			p, err := aws.NewLoadBalancerDriver(aws.CreateELBClient(aws.Credentials(nil), *elbOptions), name)
+			if err != nil {
+				return err
+			}
+
+			ext := loadbalancer.ProtocolFromString(publishOptions.Protocol)
+			backend := ext
+
+			result, err := p.PublishService(ext, publishOptions.ExtPort, backend, publishOptions.BackendPort)
+			if err != nil {
+				return err
+			}
+			fmt.Println(result)
+			return nil
+		},
+	}
+	publishCmd.Flags().Uint32Var(&publishOptions.ExtPort, "ext_port", 80, "External port")
+	publishCmd.Flags().Uint32Var(&publishOptions.BackendPort, "backend_port", 30000, "Backend port")
+	publishCmd.Flags().StringVar(&publishOptions.Protocol, "protocol", "http", "Protocol: http|https|tcp|tls")
+
+	unpublishPort := uint32(80)
+	unpublishCmd := &cobra.Command{
+		Use:   "unpublish",
+		Short: "Unpublish a service at given port",
+		RunE: func(_ *cobra.Command, args []string) error {
+			name := args[0]
+
+			p, err := aws.NewLoadBalancerDriver(aws.CreateELBClient(aws.Credentials(nil), *elbOptions), name)
+			if err != nil {
+				return err
+			}
+
+			result, err := p.UnpublishService(unpublishPort)
+			if err != nil {
+				return err
+			}
+			fmt.Println(result)
+			return nil
+		},
+	}
+	unpublishCmd.Flags().Uint32Var(&unpublishPort, "ext_port", unpublishPort, "External port")
+
+	cmd.AddCommand(describeCmd, registerCmd, deregisterCmd, publishCmd, unpublishCmd)
+
+	return cmd
+}
---
+++ controller/loadbalancer/cmd/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/docker/docker/opts"
+	"github.com/docker/go-connections/tlsconfig"
+	"github.com/spf13/cobra"
+)
+
+var (
+	host       = opts.DefaultHost
+	tlsOptions = tlsconfig.Options{}
+	logLevel   = len(log.AllLevels) - 1
+)
+
+func main() {
+	cmd := &cobra.Command{
+		Use:   "loadbalancer",
+		Short: "Load Balancer Controller for Editions",
+		PersistentPreRunE: func(_ *cobra.Command, args []string) error {
+			if logLevel > len(log.AllLevels)-1 {
+				logLevel = len(log.AllLevels) - 1
+			} else if logLevel < 0 {
+				logLevel = 0
+			}
+			log.SetLevel(log.AllLevels[logLevel])
+			return nil
+		},
+	}
+
+	cmd.PersistentFlags().StringVar(&host, "host", host, "Docker host")
+	cmd.PersistentFlags().StringVar(&tlsOptions.CAFile, "tlscacert", "", "TLS CA cert")
+	cmd.PersistentFlags().StringVar(&tlsOptions.CertFile, "tlscert", "", "TLS cert")
+	cmd.PersistentFlags().StringVar(&tlsOptions.KeyFile, "tlskey", "", "TLS key")
+	cmd.PersistentFlags().BoolVar(&tlsOptions.InsecureSkipVerify, "tlsverify", true, "True to skip TLS")
+	cmd.PersistentFlags().IntVar(&logLevel, "log", logLevel, "Logging level. 0 is least verbose. Max is 5")
+
+	cmd.AddCommand(runCommand(), elbCommand(), albCommand(), dockerCommand())
+
+	err := cmd.Execute()
+	if err != nil {
+		panic(err)
+	}
+}
