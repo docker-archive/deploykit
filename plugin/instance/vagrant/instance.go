18516f3780c337df5bbae70a145c69690ead4ce4
Bill Farner <bill@docker.com>
2016-09-22 15:01:27 -0700

    Omit vagrant test main

--- plugin/instance/vagrant/instance.go
+++ plugin/instance/vagrant/instance.go
@@ -16,7 +16,7 @@ import (
 
 const vagrantFile = `
 Vagrant.configure("2") do |config|
-  config.vm.box = "bento/ubuntu-16.04"
+  config.vm.box = "bento/ubuntu-14.04"
   config.vm.hostname = "machete.box"
   config.vm.network "private_network"{{.NETWORK_OPTIONS}}
 
