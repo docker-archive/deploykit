83cff444d13e315609a702acad21c7deef4f96b9
David Chung <david.chung@docker.com>
2016-10-26 19:29:34 -0700

    lint fixes
    Signed-off-by: David Chung <david.chung@docker.com>

--- rpc/group/service.go
+++ rpc/group/service.go
@@ -14,6 +14,7 @@ type Group struct {
 	plugin group.Plugin
 }
 
+// WatchGroup is the rpc method to watch a group
 func (p *Group) WatchGroup(req *WatchGroupRequest, resp *WatchGroupResponse) error {
 	err := p.plugin.WatchGroup(req.Spec)
 	if err != nil {
@@ -23,6 +24,7 @@ func (p *Group) WatchGroup(req *WatchGroupRequest, resp *WatchGroupResponse) err
 	return nil
 }
 
+// UnwatchGroup is the rpc method to unwatch a group
 func (p *Group) UnwatchGroup(req *UnwatchGroupRequest, resp *UnwatchGroupResponse) error {
 	err := p.plugin.UnwatchGroup(req.ID)
 	if err != nil {
@@ -32,6 +34,7 @@ func (p *Group) UnwatchGroup(req *UnwatchGroupRequest, resp *UnwatchGroupRespons
 	return nil
 }
 
+// InspectGroup is the rpc method to inspect a group
 func (p *Group) InspectGroup(req *InspectGroupRequest, resp *InspectGroupResponse) error {
 	desc, err := p.plugin.InspectGroup(req.ID)
 	if err != nil {
@@ -41,6 +44,7 @@ func (p *Group) InspectGroup(req *InspectGroupRequest, resp *InspectGroupRespons
 	return nil
 }
 
+// DescribeUpdate is the rpc method to describe an update without performing it
 func (p *Group) DescribeUpdate(req *DescribeUpdateRequest, resp *DescribeUpdateResponse) error {
 	plan, err := p.plugin.DescribeUpdate(req.Spec)
 	if err != nil {
@@ -50,6 +54,7 @@ func (p *Group) DescribeUpdate(req *DescribeUpdateRequest, resp *DescribeUpdateR
 	return nil
 }
 
+// UpdateGroup is the rpc method to actually updating a group
 func (p *Group) UpdateGroup(req *UpdateGroupRequest, resp *UpdateGroupResponse) error {
 	err := p.plugin.UpdateGroup(req.Spec)
 	if err != nil {
@@ -59,6 +64,7 @@ func (p *Group) UpdateGroup(req *UpdateGroupRequest, resp *UpdateGroupResponse) 
 	return nil
 }
 
+// StopUpdate is the rpc method to stop a current update
 func (p *Group) StopUpdate(req *StopUpdateRequest, resp *StopUpdateResponse) error {
 	err := p.plugin.StopUpdate(req.ID)
 	if err != nil {
@@ -68,6 +74,7 @@ func (p *Group) StopUpdate(req *StopUpdateRequest, resp *StopUpdateResponse) err
 	return nil
 }
 
+// DestroyGroup is the rpc method to destroy a group
 func (p *Group) DestroyGroup(req *DestroyGroupRequest, resp *DestroyGroupResponse) error {
 	err := p.plugin.DestroyGroup(req.ID)
 	if err != nil {
--- rpc/group/types.go
+++ rpc/group/types.go
@@ -4,62 +4,78 @@ import (
 	"github.com/docker/infrakit/spi/group"
 )
 
+// WatchGroupRequest is the rpc wrapper for input to watch a group
 type WatchGroupRequest struct {
 	Spec group.Spec
 }
 
+// WatchGroupResponse is the rpc wrapper for the results to watch a group
 type WatchGroupResponse struct {
 	OK bool
 }
 
+// UnwatchGroupRequest is the rpc wrapper for input to unwatch a group
 type UnwatchGroupRequest struct {
 	ID group.ID
 }
 
+// UnwatchGroupResponse is the rpc wrapper for the results to unwatch a group
 type UnwatchGroupResponse struct {
 	OK bool
 }
 
+// InspectGroupRequest is the rpc wrapper for the input to inspect a group
 type InspectGroupRequest struct {
 	ID group.ID
 }
 
+// InspectGroupResponse is the rpc wrapper for the results from inspecting a group
 type InspectGroupResponse struct {
 	Description group.Description
 }
 
+// DescribeUpdateRequest is the rpc wrapper for the input to describe an update
 type DescribeUpdateRequest struct {
 	Spec group.Spec
 }
 
+// DescribeUpdateResponse is the rpc wrapper for the results from describing an update
 type DescribeUpdateResponse struct {
 	Plan string
 }
 
+// UpdateGroupRequest is the rpc wrapper for the input to update a group
 type UpdateGroupRequest struct {
 	Spec group.Spec
 }
 
+// UpdateGroupResponse is the rpc wrapper for the results of updating a group
 type UpdateGroupResponse struct {
 	OK bool
 }
 
+// StopUpdateRequest is the rpc wrapper for input to stop an update
 type StopUpdateRequest struct {
 	ID group.ID
 }
 
+// StopUpdateResponse is the rpc wrapper for the output from stopping an update
 type StopUpdateResponse struct {
 	OK bool
 }
 
+// DestroyGroupRequest is the rpc wrapper for the input to destroy a group
 type DestroyGroupRequest struct {
 	ID group.ID
 }
 
+// DestroyGroupResponse is the rpc wrapper for the output from destroying a group
 type DestroyGroupResponse struct {
 	OK bool
 }
 
+// RPCService is the interface for exposing the group plugin as a RPC service. It conforms to the call conventions
+// defined in net/rpc
 type RPCService interface {
 	WatchGroup(req *WatchGroupRequest, resp *WatchGroupResponse) error
 	UnwatchGroup(req *UnwatchGroupRequest, resp *UnwatchGroupResponse) error
