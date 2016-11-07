5b1ac18977270f3f519b3258118ce6695705db19
David Chung <david.chung@docker.com>
2016-11-07 17:50:06 -0600

    Leadership API + polling implementations (#280)
    Signed-off-by: David Chung <david.chung@docker.com>

---
+++ leader/leader.go
@@ -0,0 +1,44 @@
+package leader
+
+// Status indicates leadership status
+type Status int
+
+const (
+	// NotLeader means the current node is not a leader
+	NotLeader Status = iota
+
+	// Leader means the current node / instance is a leader
+	Leader
+
+	// Unknown indicates some exception happened while determining leadership.  Consumer will interpret accordingly.
+	Unknown
+)
+
+// CheckLeaderFunc is all that a special backend needs to implement.  It can be used with the
+// NewPoller function to return a polling implementation of the Detector interface.
+// This function returns true or false for leadership when there are no errors.  Returned error is reported and
+// the status of the event will be set to `Unknown`.
+type CheckLeaderFunc func() (bool, error)
+
+// Leadership is a struct that captures the leadership state, possibly error if exception occurs
+type Leadership struct {
+	Status Status
+	Error  error
+}
+
+// Detector is the interface for determining whether this instance is a leader
+type Detector interface {
+
+	// Start starts leadership detection
+	Start() (<-chan Leadership, error)
+
+	// Stop stops
+	Stop()
+}
+
+// Always is a trivial implementation that asserts the current instance to always be the leader (or not)
+func Always(leader bool) CheckLeaderFunc {
+	return func() (bool, error) {
+		return leader, nil
+	}
+}
