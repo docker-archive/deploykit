412875613b17a0c39c9ffdf5567b43ae7e7359fe
David Gageot <david@gageot.net>
2016-12-08 11:55:05 -0800

    Fix the default name of the instance plugin
    Signed-off-by: David Gageot <david@gageot.net>

--- plugin/instance/cmd/main.go
+++ plugin/instance/cmd/main.go
@@ -16,7 +16,7 @@ func main() {
 		Short: "GCE instance plugin",
 	}
 
-	name := cmd.Flags().String("name", "instance-gce", "Plugin name to advertise for discovery")
+	name := cmd.Flags().String("name", "instance-gcp", "Plugin name to advertise for discovery")
 	logLevel := cmd.Flags().Int("log", cli.DefaultLogLevel, "Logging level. 0 is least verbose. Max is 5")
 	project := cmd.Flags().String("project", "", "Google Cloud project")
 	zone := cmd.Flags().String("zone", "europe-west1-d", "Google Cloud zone")
