7b056fb893632827f86d17a04b35487bf75a7610
David Chung <david.chung@docker.com>
2016-12-12 11:41:34 -0800

    Ensure plugin discovery directory is in place  (#329)
    Signed-off-by: David Chung <david.chung@docker.com>

--- pkg/cli/serverutil.go
+++ pkg/cli/serverutil.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"os"
 	"path"
 
 	log "github.com/Sirupsen/logrus"
@@ -8,12 +9,23 @@ import (
 	"github.com/docker/infrakit/pkg/rpc/server"
 )
 
+// EnsureDirExists makes sure the directory where the socket file will be placed exists.
+func EnsureDirExists(dir string) {
+	os.MkdirAll(dir, 0700)
+}
+
 // RunPlugin runs a plugin server, advertising with the provided name for discovery.
 // The plugin should conform to the rpc call convention as implemented in the rpc package.
 func RunPlugin(name string, plugin server.VersionedInterface) {
-	stoppable, err := server.StartPluginAtPath(path.Join(discovery.Dir(), name), plugin)
+
+	dir := discovery.Dir()
+	EnsureDirExists(dir)
+
+	stoppable, err := server.StartPluginAtPath(path.Join(dir, name), plugin)
 	if err != nil {
 		log.Error(err)
 	}
-	stoppable.AwaitStopped()
+	if stoppable != nil {
+		stoppable.AwaitStopped()
+	}
 }
