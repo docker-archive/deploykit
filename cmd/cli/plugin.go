dac39a01d7dc87c19a809b9937528f0c02d294c2
David Chung <david.chung@docker.com>
2017-01-10 00:47:40 -0800

    Add cli 'start' to launch plugins in os mode
    Signed-off-by: David Chung <david.chung@docker.com>

--- cmd/cli/plugin.go
+++ cmd/cli/plugin.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"sync"
+
 	"github.com/docker/infrakit/pkg/discovery"
+	"github.com/docker/infrakit/pkg/launch"
+	"github.com/docker/infrakit/pkg/launch/os"
+	"github.com/docker/infrakit/pkg/template"
 	"github.com/spf13/cobra"
 )
 
@@ -13,7 +18,7 @@ func pluginCommand(plugins func() discovery.Plugins) *cobra.Command {
 		Short: "Manage plugins",
 	}
 
-	ls := cobra.Command{
+	ls := &cobra.Command{
 		Use:   "ls",
 		Short: "List available plugins",
 	}
@@ -34,7 +39,89 @@ func pluginCommand(plugins func() discovery.Plugins) *cobra.Command {
 		return nil
 	}
 
-	cmd.AddCommand(&ls)
+	start := &cobra.Command{
+		Use:   "start",
+		Short: "Start named plugins. Args are a list of plugin names",
+	}
+
+	configURL := start.Flags().String("config-url", "", "URL for the startup configs")
+	osExec := start.Flags().Bool("os", false, "True to use os plugin binaries")
+
+	start.RunE = func(c *cobra.Command, args []string) error {
+
+		configTemplate, err := template.NewTemplate(*configURL, template.Options{
+			SocketDir: discovery.Dir(),
+		})
+		if err != nil {
+			return err
+		}
+
+		view, err := configTemplate.Render(nil)
+		if err != nil {
+			return err
+		}
+
+		configs := launch.Config([]byte(view))
+
+		parsedRules := []launch.Rule{}
+		err = configs.Unmarshal(&parsedRules)
+		if err != nil {
+			return err
+		}
+
+		monitors := []*launch.Monitor{}
+
+		if *osExec {
+			exec, err := os.NewLauncher(os.DefaultLogDir())
+			if err != nil {
+				return err
+			}
+			monitors = append(monitors, launch.NewMonitor(exec, parsedRules))
+		}
+
+		input := []chan<- launch.StartPlugin{}
+		for _, m := range monitors {
+			ch, err := m.Start()
+			if err != nil {
+				return err
+			}
+			input = append(input, ch)
+		}
+
+		var wait sync.WaitGroup
+
+		// now start all the plugins
+		for _, plugin := range args {
+			fmt.Println("Starting up", plugin)
+
+			wait.Add(1)
+
+			for _, ch := range input {
+
+				name := plugin
+				ch <- launch.StartPlugin{
+					Plugin: name,
+					Started: func(config *launch.Config) {
+						fmt.Println(name, "started.")
+						wait.Done()
+					},
+					Error: func(config *launch.Config, err error) {
+						fmt.Println("Error starting", name, "err=", err)
+						wait.Done()
+					},
+				}
+			}
+		}
+
+		wait.Wait() // wait for everyone to complete
+
+		for _, monitor := range monitors {
+			monitor.Stop()
+		}
+		return nil
+	}
+
+	cmd.AddCommand(ls, start)
 
 	return cmd
 }
