41ea987c19169ea5dcc084082bc0bf4a2dc8f9db
David Chung <david.chung@docker.com>
2017-01-18 11:50:59 -0800

    comment on design
    Signed-off-by: David Chung <david.chung@docker.com>

--- pkg/manager/spec.go
+++ pkg/manager/spec.go
@@ -7,5 +7,10 @@ import (
 
 // globalSpec is a simple model of a collection of Group plugin configs
 type globalSpec struct {
+
+	// Groups is a map keyed by the group.ID which is a nested field inside the spec's Properties.
+	// This is not a representation to be used externally because external, user-facing representation
+	// should be a list of plugin.Spec and not map to enforce the invariant that all YAML/JSON keys fields are
+	// fields in objects.  See https://github.com/kubernetes/community/blob/master/contributors/devel/api-conventions.md#lists-of-named-subobjects-preferred-over-maps
 	Groups map[group.ID]plugin.Spec
 }
