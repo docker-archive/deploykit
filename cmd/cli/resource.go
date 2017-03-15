7b74eebaf463f46366e5dab22e5e95a9ee984461
Noah Treuhaft <noah.treuhaft@docker.com>
2017-03-14 17:14:20 -0700

    Revert "Remove resourceIdentityFunc"
    This reverts commit e3196bcf4fa4d1664daa239f559454c1e15bdddb.
    
    Signed-off-by: Noah Treuhaft <noah.treuhaft@docker.com>

--- cmd/cli/resource.go
+++ cmd/cli/resource.go
@@ -65,7 +65,7 @@ func resourceCommand(plugins func() discovery.Plugins) *cobra.Command {
 		if err != nil {
 			return err
 		}
-		view, err := engine.Render(nil)
+		view, err := engine.AddFunc("resource", resourceIdentityFunc).Render(nil)
 		if err != nil {
 			return err
 		}
@@ -112,7 +112,7 @@ func resourceCommand(plugins func() discovery.Plugins) *cobra.Command {
 		if err != nil {
 			return err
 		}
-		view, err := engine.Render(nil)
+		view, err := engine.AddFunc("resource", resourceIdentityFunc).Render(nil)
 		if err != nil {
 			return err
 		}
@@ -138,4 +138,8 @@ func resourceCommand(plugins func() discovery.Plugins) *cobra.Command {
 	cmd.AddCommand(&destroy)
 
 	return cmd
+}
+
+func resourceIdentityFunc(name string) string {
+	return fmt.Sprintf("{{ resource `%s` }}", name)
 }
