06bc4f6adf6782c73aa9b4610bc35141dcfb3b4f
David Chung <david.chung@docker.com>
2017-03-24 00:08:33 -0700

    logging module
    Signed-off-by: David Chung <david.chung@docker.com>

--- pkg/template/integration_test.go
+++ pkg/template/integration_test.go
@@ -13,7 +13,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-var logger = log.New("template", "core")
+var logger = log.New("module", "template")
 
 func TestTemplateInclusionFromDifferentSources(t *testing.T) {
 	prefix := testSetupTemplates(t, testFiles)
