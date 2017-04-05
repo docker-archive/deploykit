91ef548c0e609507aaeede857c2b98b9666045e5
David Chung <david.chung@docker.com>
2017-04-05 00:45:50 -0700

    yaml output for metadata; better output in instance command
    Signed-off-by: David Chung <david.chung@docker.com>

--- cmd/cli/base/output.go
+++ cmd/cli/base/output.go
@@ -39,3 +39,54 @@ func RawOutput() (*pflag.FlagSet, RawOutputFunc) {
 		return true, nil
 	}
 }
+
+// OutputFunc is a function that writes some data to the output writer
+type OutputFunc func(w io.Writer, v interface{}) (rendered bool, err error)
+
+// Output returns the flagset and the func for printing output
+func Output() (*pflag.FlagSet, OutputFunc) {
+
+	fs := pflag.NewFlagSet("output", pflag.ExitOnError)
+
+	yamlDoc := fs.BoolP("yaml", "y", false, "True if input is in yaml format; json is the default")
+	return fs, func(w io.Writer, v interface{}) (rendered bool, err error) {
+
+		var out string
+
+		switch v := v.(type) {
+		case string:
+			if *yamlDoc {
+				if y, err := yaml.JSONToYAML([]byte(v)); err == nil {
+					out = string(y)
+				}
+			} else {
+				out = v
+			}
+		case []byte:
+			if *yamlDoc {
+				if y, err := yaml.JSONToYAML(v); err == nil {
+					out = string(y)
+				}
+			} else {
+				out = string(v)
+			}
+		default:
+			any, err := types.AnyValue(v)
+			if err != nil {
+				return false, err
+			}
+
+			buff := any.Bytes()
+			if *yamlDoc {
+				if y, err := yaml.JSONToYAML(buff); err == nil {
+					out = string(y)
+				}
+			} else {
+				out = any.String()
+			}
+		}
+
+		fmt.Fprintln(w, out)
+		return true, nil
+	}
+}
