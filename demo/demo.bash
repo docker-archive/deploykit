1b67d997ca5dcca3f1f091953e25177e954a9dc4
Aaron Spiegel <spiegela@gmail.com>
2017-04-12 17:13:31 -0500

    Update demo

--- demo/demo.bash
+++ demo/demo.bash
@@ -1,9 +1,12 @@
-
-
 vagrant up
 
-export TOKEN=`curl -sk -H "Content-Type: application/json" -X POST https://:9093/login -d '{"username": "admin", "password": "admin123"}' | jq '.["token"]' | sed 's|"||g'`
+if [ ! -f ./CentOS-7-x86_64-DVD-1611.iso ]
+then
+    wget http://mirrors.usc.edu/pub/linux/distributions/centos/7/isos/x86_64/CentOS-7-x86_64-DVD-1611.iso
+fi
+
+export TOKEN=`curl -sk -H "Content-Type: application/json" -X POST -d '{"username": "admin", "password": "admin123"}' https://localhost:9093/login | jq '.["token"]' | sed 's|"||g'`
 
 echo "Create VirtualBox SKU in RackHD"
 
-curl -sk -H "Authorization: JWT $TOKEN" -H "Content-Type: application/json" -X POST https://:9093/api/current/skus -d '{"name": "VirtualBox", "rules": [{"path": "dmi.Base Board Information.Product Name", "equals": "VirtualBox"}]}'
+curl -sk -H "Authorization: JWT $TOKEN" -H "Content-Type: application/json" -X POST -d '{ "name": "VirtualBox", "rules": [ { "path": "dmi.Base Board Information.Product Name", "equals": "VirtualBox" } ] }' https://localhost:9093/api/current/skus
