b4c8b73ede67b9f39ffee68fe35f4af3d50f1471
Vincent Demeester <vincent@sbr.pm>
2017-04-14 17:51:42 +0200

    Add simple unit tests to update test-related vendors
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- plugin/instance/plugin_test.go
+++ plugin/instance/plugin_test.go
@@ -1,95 +1,22 @@
 package instance
 
-//func TestUsage(t *testing.T) {
-//	run(t, `
-//{
-//            "ami" : "${lookup(var.aws_amis, var.aws_region)}",
-//            "instance_type" : "m1.small",
-//            "key_name": "PUBKEY",
-//            "vpc_security_group_ids" : ["${aws_security_group.default.id}"],
-//            "subnet_id": "${aws_subnet.default.id}"
-//}`)
-//}
-//
-//func run(t *testing.T, properties string) {
-//	dir, err := ioutil.TempDir("", "infrakit-instance-file")
-//	require.NoError(t, err)
-//
-//	defer os.RemoveAll(dir)
-//
-//	fileinst := NewDOInstancePlugin(dir)
-//
-//	config := types.AnyString(properties)
-//
-//	err = fileinst.Validate(config)
-//	require.NoError(t, err)
-//
-//	instanceSpec := instance.Spec{
-//		Properties: config,
-//		Tags: map[string]string{
-//			"label1": "value1",
-//			"label2": "value2",
-//		},
-//		Init: "apt-get update -y\n\napt-get install -y software",
-//		Attachments: []instance.Attachment{
-//			{
-//				ID:   "ebs1",
-//				Type: "ebs",
-//			},
-//		},
-//	}
-//
-//	id, err := fileinst.Provision(instanceSpec)
-//	require.NoError(t, err)
-//
-//	tfPath := filepath.Join(dir, string(*id))
-//	buff, err := ioutil.ReadFile(tfPath)
-//	require.NoError(t, err)
-//
-//	any := types.AnyBytes(buff)
-//	parsed := fileInstance{}
-//	err = any.Decode(&parsed)
-//	require.NoError(t, err)
-//	require.Equal(t, map[string]string{
-//		"label1": "value1",
-//		"label2": "value2",
-//	}, parsed.Description.Tags)
-//	require.Equal(t, instanceSpec.Init, parsed.Spec.Init)
-//
-//	// label resources
-//	err = fileinst.Label(*id, map[string]string{
-//		"label1": "changed1",
-//		"label3": "value3",
-//	})
-//
-//	buff, err = ioutil.ReadFile(tfPath)
-//	require.NoError(t, err)
-//
-//	any = types.AnyBytes(buff)
-//	parsed = fileInstance{}
-//	err = any.Decode(&parsed)
-//	require.NoError(t, err)
-//
-//	require.Equal(t, map[string]string{
-//		"label1": "changed1",
-//		"label2": "value2",
-//		"label3": "value3",
-//	}, parsed.Description.Tags)
-//
-//	list, err := fileinst.DescribeInstances(map[string]string{"label1": "changed1"}, false)
-//	require.NoError(t, err)
-//	require.Equal(t, []instance.Description{
-//		{
-//			ID:   *id,
-//			Tags: parsed.Description.Tags,
-//		},
-//	}, list)
-//
-//	err = fileinst.Destroy(*id)
-//	require.NoError(t, err)
-//
-//	list, err = fileinst.DescribeInstances(map[string]string{"label1": "changed1"}, false)
-//	require.NoError(t, err)
-//	require.Equal(t, []instance.Description{}, list)
-//
-//}
+import (
+	"testing"
+
+	"github.com/docker/infrakit/pkg/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidate(t *testing.T) {
+	plugin := &plugin{}
+	err := plugin.Validate(types.AnyString(`{"Size":"1gb", "Image": "debian-8-x64"}`))
+
+	require.NoError(t, err)
+}
+
+func TestValidateFails(t *testing.T) {
+	plugin := &plugin{}
+	err := plugin.Validate(types.AnyString("-"))
+
+	require.Error(t, err)
+}
