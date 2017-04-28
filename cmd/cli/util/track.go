7e2944fe6dca790ae87cc6e49d628e73d4430b5d
David Chung <david.chung@docker.com>
2017-04-27 19:30:34 -0700

    instance tracker
    Signed-off-by: David Chung <david.chung@docker.com>

---
+++ cmd/cli/util/track.go
@@ -0,0 +1,75 @@
+package util
+
+import (
+	"os"
+	"strings"
+	"time"
+
+	"github.com/docker/infrakit/pkg/cli"
+	"github.com/docker/infrakit/pkg/discovery"
+	"github.com/docker/infrakit/pkg/plugin"
+	"github.com/docker/infrakit/pkg/plugin/event/instance"
+	event_rpc "github.com/docker/infrakit/pkg/rpc/event"
+	instance_rpc "github.com/docker/infrakit/pkg/rpc/instance"
+	"github.com/docker/infrakit/pkg/spi/event"
+	"github.com/spf13/cobra"
+)
+
+func trackCommand(plugins func() discovery.Plugins) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "track",
+		Short: "Track instances",
+	}
+
+	name := cmd.Flags().String("name", "", "Name to use as name of this plugin")
+	targets := cmd.Flags().StringSliceP("instance", "n", []string{}, "Instance plugins to track")
+	poll := cmd.Flags().DurationP("poll", "i", 3*time.Second, "Polling interval")
+	flagTags := cmd.Flags().StringSliceP("tag", "t", []string{}, "Tags to filter instance by")
+
+	cmd.RunE = func(c *cobra.Command, args []string) error {
+
+		if len(args) != 0 {
+			cmd.Usage()
+			os.Exit(-1)
+		}
+
+		tags := map[string]string{}
+
+		for _, tag := range *flagTags {
+			kv := strings.SplitN(tag, "=", 2)
+			if len(kv) != 2 {
+				logger.Warn("bad format tag", "input", tag)
+				continue
+			}
+			key := strings.TrimSpace(kv[0])
+			val := strings.TrimSpace(kv[1])
+			if key != "" && val != "" {
+				tags[key] = val
+			}
+		}
+
+		trackers := map[string]event.Plugin{}
+
+		for _, target := range *targets {
+
+			endpoint, err := plugins().Find(plugin.Name(target))
+			if err != nil {
+				return err
+			}
+
+			if p, err := instance_rpc.NewClient(plugin.Name(target), endpoint.Address); err == nil {
+				trackers[target] = instance.NewTracker(target, p, time.Tick(*poll), tags)
+			} else {
+				return err
+			}
+		}
+
+		cli.RunPlugin(*name,
+			// As event plugin
+			event_rpc.PluginServerWithTypes(trackers),
+		)
+		return nil
+	}
+
+	return cmd
+}
