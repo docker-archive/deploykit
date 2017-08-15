b84a72fcbefd456dbc2088796e5e291b28b2804b
David Chung <david.chung@docker.com>
2017-08-15 10:07:51 -0700

    SSH tunnel for secure connection to remote Infrakit daemon (#654)
    Signed-off-by: David Chung <david.chung@docker.com>

--- pkg/cli/remotes.go
+++ pkg/cli/remotes.go
@@ -3,44 +3,64 @@ package cli
 import (
 	"fmt"
 	"io/ioutil"
+	"net"
 	"net/url"
 	"os"
-	"os/user"
 	"path/filepath"
 	"strings"
+	"sync"
 
+	"github.com/docker/infrakit/pkg/run"
 	"github.com/docker/infrakit/pkg/types"
+	"github.com/docker/infrakit/pkg/util/ssh"
 )
 
 const (
-	// HostsFileEnvVar is the location of the hosts file
-	HostsFileEnvVar = "INFRAKIT_HOSTS_FILE"
+	// EnvInfrakitHost is the environment variable to set to point to specific backends.
+	// The value is used as key into the $INFRAKIT_HOME/hosts file.
+	EnvInfrakitHost = "INFRAKIT_HOST"
+	// EnvHostsFile is the location of the hosts file
+	EnvHostsFile = "INFRAKIT_HOSTS_FILE"
 )
 
 // HostsFile returns the hsots file used for looking up hosts
 func HostsFile() string {
-	if hostsFile := os.Getenv(HostsFileEnvVar); hostsFile != "" {
-		return hostsFile
-	}
+	return run.GetEnv(EnvHostsFile, filepath.Join(run.InfrakitHome(), "hosts"))
+}
 
-	// if there's INFRAKIT_HOME defined
-	home := os.Getenv("INFRAKIT_HOME")
-	if home != "" {
-		return filepath.Join(home, "hosts")
-	}
+// Remote is a remote infrakit endpoint
+type Remote struct {
+	Endpoints HostList
+	SSH       string // The bastion host
+	User      string
+}
 
-	home = os.Getenv("HOME")
-	if usr, err := user.Current(); err == nil {
-		home = usr.HomeDir
+// parse the , delimited string into a url list
+func (r Remote) endpoints() ([]*url.URL, error) {
+	ulist := []*url.URL{}
+	for _, h := range strings.Split(string(r.Endpoints), ",") {
+		addProtocol := false
+		if !strings.Contains(h, "://") {
+			h = "http://" + h
+			addProtocol = true
+		}
+		u, err := url.Parse(h)
+		if err != nil {
+			return nil, err
+		}
+		if addProtocol {
+			u.Scheme = "http"
+		}
+		ulist = append(ulist, u)
 	}
-	return filepath.Join(home, ".infrakit/hosts")
+	return ulist, nil
 }
 
 // HostList is a comma-delimited list of protocol://host:port
 type HostList string
 
 // Hosts is the schema of the hosts file
-type Hosts map[string]HostList
+type Hosts map[string]Remote
 
 // Save saves the hosts
 func (h Hosts) Save() error {
@@ -79,9 +99,8 @@ func LoadHosts() (Hosts, error) {
 func Remotes() ([]*url.URL, error) {
 	ulist := []*url.URL{}
 
-	hosts := []string{}
 	// See if INFRAKIT_HOST is set to point to a host list in the $INFRAKIT_HOME/hosts file.
-	host := os.Getenv("INFRAKIT_HOST")
+	host := os.Getenv(EnvInfrakitHost)
 	if host == "" {
 		return ulist, nil // do nothing -- local mode
 	}
@@ -95,7 +114,7 @@ func Remotes() ([]*url.URL, error) {
 		return ulist, nil // do nothing -- local mode
 	}
 
-	m := map[string]string{}
+	m := Hosts{}
 	yaml, err := types.AnyYAML(buff)
 	if err != nil {
 		return nil, fmt.Errorf("bad format for hosts file at %s for INFRAKIT_HOST=%s, err=%v", hostsFile, host, err)
@@ -105,27 +124,88 @@ func Remotes() ([]*url.URL, error) {
 		return nil, fmt.Errorf("cannot decode hosts file at %s for INFRAKIT_HOST=%s, err=%v", hostsFile, host, err)
 	}
 
-	if list, has := m[host]; has {
-		hosts = strings.Split(list, ",")
-	} else {
+	remote, has := m[host]
+	if !has {
 		return nil, fmt.Errorf("no entry in hosts file at %s for INFRAKIT_HOST=%s", hostsFile, host)
 	}
 
-	for _, h := range hosts {
-		addProtocol := false
-		if !strings.Contains(h, "://") {
-			h = "http://" + h
-			addProtocol = true
-		}
-		u, err := url.Parse(h)
-		if err != nil {
-			panic(err)
-		}
-		if addProtocol {
-			u.Scheme = "http"
-		}
-		ulist = append(ulist, u)
+	if remote.SSH != "" {
+		return urlFromTunnel(host, remote)
+	}
+	return remote.endpoints()
+}
+
+var (
+	tunnels     = map[string]*site{}
+	tunnelsLock = sync.Mutex{}
+)
+
+type site struct {
+	rule    Remote
+	urls    []*url.URL
+	tunnels []*ssh.Tunnel
+}
+
+func (r *site) startTunnels() ([]*url.URL, error) {
+	endpoints, err := r.rule.endpoints()
+	if err != nil {
+		return nil, err
 	}
 
-	return ulist, nil
+	// endpoints are urls.  url.Host is host:port.
+	for _, u := range endpoints {
+
+		host, port, err := net.SplitHostPort(u.Host)
+		if err != nil {
+			return nil, err
+		}
+
+		bastionHost := r.rule.SSH
+		if bastionHost == "" {
+			bastionHost = u.Host
+		}
+		if h, p, err := net.SplitHostPort(bastionHost); err == nil && p == "" {
+			bastionHost = net.JoinHostPort(h, "22") // default
+		}
+
+		config := ssh.DefaultClientConfig()
+		config.User = r.rule.User
+		tunnel := &ssh.Tunnel{
+			Local:  ssh.HostPort(fmt.Sprintf("%s:%d", "127.0.0.1", ssh.RandPort(2200, 2299))),
+			Server: ssh.HostPort(bastionHost),
+			Remote: ssh.HostPort(net.JoinHostPort(host, port)),
+			Config: &config,
+		}
+
+		err = tunnel.Start()
+		if err != nil {
+			return nil, err
+		}
+
+		r.tunnels = append(r.tunnels, tunnel)
+		r.urls = append(r.urls, &url.URL{
+			Scheme: "http",
+			Host:   string(tunnel.Local),
+		})
+	}
+	return r.urls, nil
+}
+
+func urlFromTunnel(host string, remote Remote) ([]*url.URL, error) {
+	tunnelsLock.Lock()
+	defer tunnelsLock.Unlock()
+
+	t, has := tunnels[host]
+	if has {
+		return t.urls, nil
+	}
+	tunnels[host] = &site{
+		rule:    remote,
+		urls:    []*url.URL{},
+		tunnels: []*ssh.Tunnel{},
+	}
+	if remote.SSH != "" {
+		return tunnels[host].startTunnels()
+	}
+	return nil, nil
 }
---
+++ pkg/util/ssh/tunnel.go
@@ -0,0 +1,108 @@
+package ssh
+
+import (
+	"fmt"
+	"io"
+	"math/rand"
+	"net"
+	"os"
+	"time"
+
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/agent"
+)
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
+
+// HostPort is host:port
+type HostPort string
+
+// Tunnel is a forwarder of local ssh traffic to a remote endpoint
+type Tunnel struct {
+	// Local is the local endpoint for all clients
+	Local HostPort
+	// Server is the middle server listening on SSH port
+	Server HostPort
+	// Remote is the backend that's not accessible without going through the server endpoint
+	Remote HostPort
+
+	Config *ssh.ClientConfig
+}
+
+// RandPort picks a port from the given range randomly.  It doesn't check if a port has been allocated.
+func RandPort(lo, hi int) int {
+	return lo + rand.Intn(hi-lo)
+}
+
+// DefaultClientConfig returns the default settings of the ssh client
+func DefaultClientConfig() ssh.ClientConfig {
+	return ssh.ClientConfig{
+		Auth: []ssh.AuthMethod{
+			Agent(),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(), // TODO - add ways to set host's public key
+	}
+}
+
+// Start starts the tunnel
+func (tunnel *Tunnel) Start() error {
+	return <-tunnel.startAsync()
+}
+
+func (tunnel *Tunnel) startAsync() <-chan error {
+	ready := make(chan error, 1)
+	go func() {
+		listener, err := net.Listen("tcp", string(tunnel.Local))
+		if err != nil {
+			ready <- err
+			close(ready)
+			return
+		}
+		defer listener.Close()
+		close(ready) // notify caller before we block
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				panic(err)
+			}
+			go tunnel.forward(conn)
+		}
+	}()
+	return ready
+}
+
+func (tunnel *Tunnel) forward(localConn net.Conn) {
+	serverConn, err := ssh.Dial("tcp", string(tunnel.Server), tunnel.Config)
+	if err != nil {
+		fmt.Printf("Server dial error: %s\n", err)
+		localConn.Close()
+		return
+	}
+
+	remoteConn, err := serverConn.Dial("tcp", string(tunnel.Remote))
+	if err != nil {
+		fmt.Printf("Remote dial error: %s\n", err)
+		localConn.Close()
+		return
+	}
+
+	copyConn := func(writer, reader net.Conn) {
+		_, err := io.Copy(writer, reader)
+		if err != nil {
+			fmt.Printf("io.Copy error: %s", err)
+		}
+	}
+
+	go copyConn(localConn, remoteConn)
+	go copyConn(remoteConn, localConn)
+}
+
+// Agent returns the auth method using SSH agent
+func Agent() ssh.AuthMethod {
+	if sshAgent, err := net.Dial("unix", os.Getenv("SSH_AUTH_SOCK")); err == nil {
+		return ssh.PublicKeysCallback(agent.NewClient(sshAgent).Signers)
+	}
+	return nil
+}
