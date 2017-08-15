466f412b6f2a03ee022023783b0bd361d137dde3
David Chung <david.chung@docker.com>
2017-08-15 15:43:40 -0700

    Improve logging (#655)
    Signed-off-by: David Chung <david.chung@docker.com>

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -2,7 +2,9 @@ package log
 
 import (
 	"flag"
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/Sirupsen/logrus"
 	"gopkg.in/inconshreveable/log15.v2"
@@ -28,7 +30,20 @@ type Options struct {
 	Format    string
 	CallFunc  bool
 	CallStack bool
+
+	// DebugMatchKeyValuePairs is the '=' delimited kv pair for filtering contexts in records for DEBUG
+	DebugMatchKeyValuePairs []string
+
+	// DebugMatchExclude excludes the record if any of the context params matches those in the MatchKeyValuePairs
+	DebugMatchExclude bool
+
+	// DebugV is a value of verbosity for checking debug records where there's a context param of type DebugV
+	DebugV int
 }
+
+// V is the Verbosity level.  To set a verbosity level, just put a log.V(100) in the context
+// (e.g. .Debug(msg, "key", log.V(100))
+type V int
 
 // DevDefaults is the default options for development
 var DevDefaults = Options{
@@ -103,7 +118,9 @@ func Configure(options *Options) {
 	case 4:
 		h = log15.LvlFilterHandler(log15.LvlInfo, h)
 	case 5:
-		h = log15.LvlFilterHandler(log15.LvlDebug, h)
+		h = log15.LvlFilterHandler(log15.LvlDebug,
+			verbosityFilterHandler(V(options.DebugV),
+				matchAnyFilterHandler(options.DebugMatchKeyValuePairs, h, !options.DebugMatchExclude)))
 	default:
 		h = log15.LvlFilterHandler(log15.LvlInfo, h)
 	}
@@ -111,4 +128,54 @@ func Configure(options *Options) {
 
 	// Necessary to stop glog from complaining / noisy logs
 	flag.CommandLine.Parse([]string{})
+}
+
+func verbosityFilterHandler(l V, h log15.Handler) log15.Handler {
+	return log15.FilterHandler(func(r *log15.Record) bool {
+		if l == 0 {
+			return true
+		}
+		for i := 0; i < len(r.Ctx); i += 2 {
+			switch v := r.Ctx[i+1].(type) {
+			case V:
+				return !(v > l)
+			default:
+				continue
+			}
+		}
+		return true
+	}, h)
+}
+
+func matchAnyFilterHandler(kv []string, h log15.Handler, log bool) log15.Handler {
+	index := map[interface{}]map[interface{}]struct{}{}
+	for _, s := range kv {
+		p := strings.Split(s, "=")
+		if len(p) != 2 {
+			fmt.Fprintf(os.Stderr, "Bad filter: %v\n", s)
+			continue
+		}
+
+		if _, has := index[p[0]]; !has {
+			index[p[0]] = map[interface{}]struct{}{}
+		}
+		index[p[0]][p[1]] = struct{}{}
+	}
+
+	return log15.FilterHandler(func(r *log15.Record) bool {
+		if len(index) == 0 {
+			return true
+		}
+
+		// find ANY of the context keys and values in the index
+		for i := 0; i < len(r.Ctx); i += 2 {
+			if m, has := index[r.Ctx[i]]; has {
+				check := fmt.Sprintf("%v", r.Ctx[i+1])
+				if _, has := m[check]; has {
+					return log
+				}
+			}
+		}
+		return !log
+	}, h)
 }
