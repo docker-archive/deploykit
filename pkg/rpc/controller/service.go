65951ac063d28ebedf7b79d6f4c52a6bc84ffe3c
David Chung <david.chung@docker.com>
2017-08-22 14:12:09 -0700

    controller now uses shared internal rpc dispatcher
    Signed-off-by: David Chung <david.chung@docker.com>

--- pkg/rpc/controller/service.go
+++ pkg/rpc/controller/service.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/docker/infrakit/pkg/controller"
@@ -9,28 +8,59 @@ import (
 	"github.com/docker/infrakit/pkg/plugin"
 	"github.com/docker/infrakit/pkg/rpc/internal"
 	"github.com/docker/infrakit/pkg/spi"
+	"github.com/docker/infrakit/pkg/types"
 )
 
 var log = logutil.New("module", "rpc/controller")
 
-// ServerWithTypes returns a Controller map
-func ServerWithTypes(subcontrollers func() (map[string]controller.Controller, error)) *Controller {
-	return &Controller{subcontrollers: subcontrollers}
+// ServerWithNamed returns a Controller map
+func ServerWithNamed(subcontrollers func() (map[string]controller.Controller, error)) *Controller {
+
+	keyed := internal.ServeKeyed(
+
+		// This is where templates would be nice...
+		func() (map[string]interface{}, error) {
+			m, err := subcontrollers()
+			if err != nil {
+				return nil, err
+			}
+			out := map[string]interface{}{}
+			for k, v := range m {
+				out[string(k)] = v
+			}
+			return out, nil
+		},
+	)
+
+	return &Controller{keyed: keyed}
 }
 
 // Server returns a Controller that conforms to the net/rpc rpc call convention.
 func Server(c controller.Controller) *Controller {
-	return ServerWithTypes(func() (map[string]controller.Controller, error) {
-		return map[string]controller.Controller{
-			".": c,
-		}, nil
-	})
+	return &Controller{keyed: internal.ServeSingle(c)}
 }
 
 // Controller is the exported type for json-rpc
 type Controller struct {
-	internal.Keyed
-	subcontrollers func() (map[string]controller.Controller, error)
+	keyed *internal.Keyed
+}
+
+// VendorInfo returns a metadata object about the plugin, if the plugin implements it.  See plugin.Vendor
+func (c *Controller) VendorInfo() *spi.VendorInfo {
+	base, _ := c.keyed.Keyed(plugin.Name("."))
+	if m, is := base.(spi.Vendor); is {
+		return m.VendorInfo()
+	}
+	return nil
+}
+
+// ExampleProperties returns an example properties used by the plugin
+func (c *Controller) ExampleProperties() *types.Any {
+	base, _ := c.keyed.Keyed(plugin.Name("."))
+	if i, is := base.(spi.InputExample); is {
+		return i.ExampleProperties()
+	}
+	return nil
 }
 
 // ImplementedInterface returns the interface implemented by this RPC service.
@@ -40,86 +70,59 @@ func (c *Controller) ImplementedInterface() spi.InterfaceSpec {
 
 // Types returns the types exposed by this kind of RPC service
 func (c *Controller) Types() []string {
-	m, err := c.subcontrollers()
-	if err != nil {
-		return nil
-	}
-	types := []string{}
-	for k := range m {
-		types = append(types, k)
-	}
-	return types
-}
-
-func (c *Controller) subController(name plugin.Name) (controller.Controller, error) {
-	m, err := c.subcontrollers()
-	if err != nil {
-		return nil, err
-	}
-	_, subtype := name.GetLookupAndType()
-	key := subtype
-	if subtype == "." || subtype == "" {
-		key = "."
-	}
-	if p, has := m[key]; has {
-		return p, nil
-	}
-	return nil, fmt.Errorf("no-controller:%v", name)
+	return c.keyed.Types()
 }
 
 // Plan is the rpc method for Plan
 func (c *Controller) Plan(_ *http.Request, req *ChangeRequest, resp *ChangeResponse) error {
-	resp.Name = req.Name
-	subcontroller, err := c.subController(req.Name)
-	if err != nil {
+
+	return c.keyed.Do(req, func(v interface{}) error {
+		resp.Name = req.Name
+		object, plan, err := v.(controller.Controller).Plan(req.Operation, req.Spec)
+		if err == nil {
+			resp.Object = object
+			resp.Plan = plan
+		}
 		return err
-	}
-	object, plan, err := subcontroller.Plan(req.Operation, req.Spec)
-	if err == nil {
-		resp.Object = object
-		resp.Plan = plan
-	}
-	return err
+	})
+
 }
 
 // Commit is the rpc method for Commit
 func (c *Controller) Commit(_ *http.Request, req *ChangeRequest, resp *ChangeResponse) error {
-	resp.Name = req.Name
-	subcontroller, err := c.subController(req.Name)
-	if err != nil {
+
+	return c.keyed.Do(req, func(v interface{}) error {
+		resp.Name = req.Name
+		object, err := v.(controller.Controller).Commit(req.Operation, req.Spec)
+		if err == nil {
+			resp.Object = object
+		}
 		return err
-	}
-	object, err := subcontroller.Commit(req.Operation, req.Spec)
-	if err == nil {
-		resp.Object = object
-	}
-	return err
+	})
 }
 
 // Describe is the rpc method for Describe
 func (c *Controller) Describe(_ *http.Request, req *FindRequest, resp *FindResponse) error {
-	resp.Name = req.Name
-	subcontroller, err := c.subController(req.Name)
-	if err != nil {
+
+	return c.keyed.Do(req, func(v interface{}) error {
+		resp.Name = req.Name
+		objects, err := v.(controller.Controller).Describe(req.Metadata)
+		if err == nil {
+			resp.Objects = objects
+		}
 		return err
-	}
-	objects, err := subcontroller.Describe(req.Metadata)
-	if err == nil {
-		resp.Objects = objects
-	}
-	return err
+	})
 }
 
 // Free is the rpc method for Free
 func (c *Controller) Free(_ *http.Request, req *FindRequest, resp *FindResponse) error {
-	resp.Name = req.Name
-	subcontroller, err := c.subController(req.Name)
-	if err != nil {
+
+	return c.keyed.Do(req, func(v interface{}) error {
+		resp.Name = req.Name
+		objects, err := v.(controller.Controller).Free(req.Metadata)
+		if err == nil {
+			resp.Objects = objects
+		}
 		return err
-	}
-	objects, err := subcontroller.Free(req.Metadata)
-	if err == nil {
-		resp.Objects = objects
-	}
-	return err
+	})
 }
