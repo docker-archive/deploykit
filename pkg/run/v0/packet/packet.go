9131a3a38c24cdc9a395fc152c711d61ae4c2906
David Chung <david.chung@docker.com>
2017-09-27 18:58:39 -0700

    moving packet to providers

--- pkg/run/v0/packet/packet.go
+++ pkg/run/v0/packet/packet.go
@@ -7,7 +7,7 @@ import (
 	"github.com/docker/infrakit/pkg/launch/inproc"
 	logutil "github.com/docker/infrakit/pkg/log"
 	"github.com/docker/infrakit/pkg/plugin"
-	"github.com/docker/infrakit/pkg/plugin/instance/packet"
+	packet "github.com/docker/infrakit/pkg/provider/packet/plugin/instance"
 	"github.com/docker/infrakit/pkg/run"
 	"github.com/docker/infrakit/pkg/run/local"
 	"github.com/docker/infrakit/pkg/spi/instance"
