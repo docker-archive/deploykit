815e3a67b7c8b641e0164858e27d52d1f948b940
David Chung <david.chung@docker.com>
2017-10-02 15:13:28 -0700

    fix broken build
    Signed-off-by: David Chung <david.chung@docker.com>

--- pkg/spi/event/types_test.go
+++ pkg/spi/event/types_test.go
@@ -43,7 +43,7 @@ func TestEncodeDecode(t *testing.T) {
 	event2 := Event{}
 	err := any2.Decode(&event2)
 	require.NoError(t, err)
-	require.Equal(t, event.Timestamp.String(), event2.Timestamp.String())
+	require.Equal(t, event.Timestamp.Unix(), event2.Timestamp.Unix())
 	require.Equal(t, event.Data, event2.Data)
 	require.Equal(t, any.Bytes(), types.AnyValueMust(event2).Bytes())
 
