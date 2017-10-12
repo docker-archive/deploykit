550c47f4dfbc12c35adacb5f7d3e8a777194ac86
David Chung <david.chung@docker.com>
2017-10-12 11:09:36 -0700

    fix broken test
    Signed-off-by: David Chung <david.chung@docker.com>

--- pkg/plugin/group/lazy_test.go
+++ pkg/plugin/group/lazy_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/docker/infrakit/pkg/spi/group"
 	"github.com/docker/infrakit/pkg/spi/instance"
 	"github.com/stretchr/testify/require"
 )
@@ -14,14 +15,14 @@ import (
 func TestLazyBlockAndCancel(t *testing.T) {
 
 	called := make(chan struct{})
-	g := LazyConnect(func() (Plugin, error) {
+	g := LazyConnect(func() (group.Plugin, error) {
 		close(called)
 		return nil, fmt.Errorf("boom")
 	}, 100*time.Second)
 
 	errs := make(chan error, 1)
 	go func() {
-		_, err := g.DescribeGroup(ID("test"))
+		_, err := g.DescribeGroup(group.ID("test"))
 		errs <- err
 		close(errs)
 	}()
@@ -36,14 +37,14 @@ func TestLazyBlockAndCancel(t *testing.T) {
 func TestLazyNoBlock(t *testing.T) {
 
 	called := make(chan struct{})
-	g := LazyConnect(func() (Plugin, error) {
+	g := LazyConnect(func() (group.Plugin, error) {
 		close(called)
 		return nil, fmt.Errorf("boom")
 	}, 0)
 
 	errs := make(chan error, 1)
 	go func() {
-		_, err := g.DescribeGroup(ID("test"))
+		_, err := g.DescribeGroup(group.ID("test"))
 		errs <- err
 		close(errs)
 	}()
@@ -55,43 +56,43 @@ func TestLazyNoBlock(t *testing.T) {
 
 type fake chan []interface{}
 
-func (f fake) CommitGroup(grp Spec, pretend bool) (string, error) {
-	f <- []interface{}{Plugin.CommitGroup, grp, pretend}
+func (f fake) CommitGroup(grp group.Spec, pretend bool) (string, error) {
+	f <- []interface{}{group.Plugin.CommitGroup, grp, pretend}
 	return "", nil
 }
 
-func (f fake) FreeGroup(id ID) error {
-	f <- []interface{}{Plugin.FreeGroup, id}
+func (f fake) FreeGroup(id group.ID) error {
+	f <- []interface{}{group.Plugin.FreeGroup, id}
 	return nil
 }
 
-func (f fake) DescribeGroup(id ID) (Description, error) {
-	f <- []interface{}{Plugin.DescribeGroup, id}
-	return Description{}, nil
+func (f fake) DescribeGroup(id group.ID) (group.Description, error) {
+	f <- []interface{}{group.Plugin.DescribeGroup, id}
+	return group.Description{}, nil
 }
 
-func (f fake) DestroyGroup(id ID) error {
-	f <- []interface{}{Plugin.DestroyGroup, id}
+func (f fake) DestroyGroup(id group.ID) error {
+	f <- []interface{}{group.Plugin.DestroyGroup, id}
 	return nil
 }
 
-func (f fake) InspectGroups() ([]Spec, error) {
-	f <- []interface{}{Plugin.InspectGroups}
+func (f fake) InspectGroups() ([]group.Spec, error) {
+	f <- []interface{}{group.Plugin.InspectGroups}
 	return nil, nil
 }
 
-func (f fake) DestroyInstances(id ID, sub []instance.ID) error {
-	f <- []interface{}{Plugin.DestroyInstances, id, sub}
+func (f fake) DestroyInstances(id group.ID, sub []instance.ID) error {
+	f <- []interface{}{group.Plugin.DestroyInstances, id, sub}
 	return nil
 }
 
-func (f fake) Size(id ID) (int, error) {
-	f <- []interface{}{Plugin.Size, id}
+func (f fake) Size(id group.ID) (int, error) {
+	f <- []interface{}{group.Plugin.Size, id}
 	return 100, nil
 }
 
-func (f fake) SetSize(id ID, sz int) error {
-	f <- []interface{}{Plugin.SetSize, id, sz}
+func (f fake) SetSize(id group.ID, sz int) error {
+	f <- []interface{}{group.Plugin.SetSize, id, sz}
 	return nil
 }
 
@@ -111,50 +112,50 @@ func TestLazyNoBlockConnect(t *testing.T) {
 	called := make(chan struct{})
 	called2 := make(chan []interface{}, 2)
 
-	g := LazyConnect(func() (Plugin, error) {
+	g := LazyConnect(func() (group.Plugin, error) {
 		close(called)
 		return fake(called2), nil
 	}, 0)
 
 	errs := make(chan error, 1)
 	go func() {
-		_, err := g.DescribeGroup(ID("test"))
+		_, err := g.DescribeGroup(group.ID("test"))
 		errs <- err
 		close(errs)
 
-		g.Size(ID("test"))
+		g.Size(group.ID("test"))
 		close(called2)
 	}()
 
 	<-called
 
 	require.NoError(t, <-errs)
-	checkCalls(t, called2, Plugin.DescribeGroup, ID("test"))
-	checkCalls(t, called2, Plugin.Size, ID("test"))
+	checkCalls(t, called2, group.Plugin.DescribeGroup, group.ID("test"))
+	checkCalls(t, called2, group.Plugin.Size, group.ID("test"))
 }
 
 func TestErrorOnCallsToNilPlugin(t *testing.T) {
 
 	errMessage := "no-plugin"
-	proxy := LazyConnect(func() (Plugin, error) {
+	proxy := LazyConnect(func() (group.Plugin, error) {
 		return nil, errors.New(errMessage)
 	}, 0)
 
-	err := proxy.FreeGroup(ID("test"))
+	err := proxy.FreeGroup(group.ID("test"))
 	require.Error(t, err)
 	require.Equal(t, errMessage, err.Error())
 }
 
 type fakeGroupPlugin struct {
-	Plugin
-	commitGroup func(grp Spec, pretend bool) (string, error)
-	freeGroup   func(id ID) error
+	group.Plugin
+	commitGroup func(grp group.Spec, pretend bool) (string, error)
+	freeGroup   func(id group.ID) error
 }
 
-func (f *fakeGroupPlugin) CommitGroup(grp Spec, pretend bool) (string, error) {
+func (f *fakeGroupPlugin) CommitGroup(grp group.Spec, pretend bool) (string, error) {
 	return f.commitGroup(grp, pretend)
 }
-func (f *fakeGroupPlugin) FreeGroup(id ID) error {
+func (f *fakeGroupPlugin) FreeGroup(id group.ID) error {
 	return f.freeGroup(id)
 }
 
@@ -162,19 +163,19 @@ func TestDelayPluginLookupCallingMethod(t *testing.T) {
 
 	called := false
 	fake := &fakeGroupPlugin{
-		commitGroup: func(grp Spec, pretend bool) (string, error) {
+		commitGroup: func(grp group.Spec, pretend bool) (string, error) {
 			called = true
-			require.Equal(t, Spec{ID: "foo"}, grp)
+			require.Equal(t, group.Spec{ID: "foo"}, grp)
 			require.Equal(t, true, pretend)
 			return "some-response", nil
 		},
 	}
 
-	proxy := LazyConnect(func() (Plugin, error) { return fake, nil }, 0)
+	proxy := LazyConnect(func() (group.Plugin, error) { return fake, nil }, 0)
 
 	require.False(t, called)
 
-	actualStr, actualErr := proxy.CommitGroup(Spec{ID: "foo"}, true)
+	actualStr, actualErr := proxy.CommitGroup(group.Spec{ID: "foo"}, true)
 	require.True(t, called)
 	require.NoError(t, actualErr)
 	require.Equal(t, "some-response", actualStr)
@@ -184,18 +185,18 @@ func TestDelayPluginLookupCallingMethodReturnsError(t *testing.T) {
 
 	called := false
 	fake := &fakeGroupPlugin{
-		freeGroup: func(id ID) error {
+		freeGroup: func(id group.ID) error {
 			called = true
-			require.Equal(t, ID("foo"), id)
+			require.Equal(t, group.ID("foo"), id)
 			return errors.New("can't-free")
 		},
 	}
 
-	proxy := LazyConnect(func() (Plugin, error) { return fake, nil }, 0)
+	proxy := LazyConnect(func() (group.Plugin, error) { return fake, nil }, 0)
 
 	require.False(t, called)
 
-	actualErr := proxy.FreeGroup(ID("foo"))
+	actualErr := proxy.FreeGroup(group.ID("foo"))
 	require.True(t, called)
 	require.Error(t, actualErr)
 	require.Equal(t, "can't-free", actualErr.Error())
@@ -205,30 +206,30 @@ func TestDelayPluginLookupCallingMultipleMethods(t *testing.T) {
 
 	called := false
 	fake := &fakeGroupPlugin{
-		commitGroup: func(grp Spec, pretend bool) (string, error) {
+		commitGroup: func(grp group.Spec, pretend bool) (string, error) {
 			called = true
-			require.Equal(t, Spec{ID: "foo"}, grp)
+			require.Equal(t, group.Spec{ID: "foo"}, grp)
 			require.Equal(t, true, pretend)
 			return "some-response", nil
 		},
-		freeGroup: func(id ID) error {
+		freeGroup: func(id group.ID) error {
 			called = true
-			require.Equal(t, ID("foo"), id)
+			require.Equal(t, group.ID("foo"), id)
 			return errors.New("can't-free")
 		},
 	}
 
-	proxy := LazyConnect(func() (Plugin, error) { return fake, nil }, 0)
+	proxy := LazyConnect(func() (group.Plugin, error) { return fake, nil }, 0)
 
 	require.False(t, called)
 
-	actualStr, actualErr := proxy.CommitGroup(Spec{ID: "foo"}, true)
+	actualStr, actualErr := proxy.CommitGroup(group.Spec{ID: "foo"}, true)
 	require.True(t, called)
 	require.NoError(t, actualErr)
 	require.Equal(t, "some-response", actualStr)
 
 	called = false
-	actualErr = proxy.FreeGroup(ID("foo"))
+	actualErr = proxy.FreeGroup(group.ID("foo"))
 	require.True(t, called)
 	require.Error(t, actualErr)
 	require.Equal(t, "can't-free", actualErr.Error())
