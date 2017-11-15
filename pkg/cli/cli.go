196a9a57329f0924179b04c19baa227bb0de88e8
David Chung <david.chung@docker.com>
2017-11-14 18:22:56 -0800

    Move dynamic playbook commands to `infrakit use` (#764)
    Signed-off-by: David Chung <david.chung@docker.com>

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -4,11 +4,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const (
-	// CliDirEnvVar is the environment variable that points to where the cli config folders are.
-	CliDirEnvVar = "INFRAKIT_CLI_DIR"
-)
-
 // Modules provides access to CLI module discovery
 type Modules interface {
 
