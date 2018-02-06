d0d12327b6749c968aeacf0366aaea2833de0b49
David Chung <david.chung@docker.com>
2018-02-05 17:39:58 -0800

    fix lint
    Signed-off-by: David Chung <david.chung@docker.com>

--- pkg/fsm/usage_gc_test.go
+++ pkg/fsm/usage_gc_test.go
@@ -9,25 +9,25 @@ import (
 )
 
 const (
-	docker_node_ready Signal = iota
-	docker_node_down
-	docker_node_gone
-	instance_ok
-	instance_gone
+	dockerNodeReady Signal = iota
+	dockerNodeDown
+	dockerNodeGone
+	instanceOK
+	instanceGone
 	timeout
 	reap
 )
 
 const (
-	start                    Index = iota
-	matched_instance               // has vm information, waiting to match to docker_node
-	matched_docker_node            // has docker_node information, waiting to match to vm
-	swarm_node                     // has matching docker_node and vm information
-	swarm_node_ready               // ready as swarm node
-	swarm_node_down                // unavailable as swarm node
-	pending_instance_destroy       // vm needs to be removed_instance (instance destroy)
-	removed_instance               // instance is deleted
-	done                           // terminal
+	start                  Index = iota
+	matchedInstance              // has vm information, waiting to match to docker_node
+	matchedDockerNode            // has docker_node information, waiting to match to vm
+	swarmNode                    // has matching docker_node and vm information
+	swarmNodeReady               // ready as swarm node
+	swarmNodeDown                // unavailable as swarm node
+	pendingInstanceDestroy       // vm needs to be removedInstance (instance destroy)
+	removedInstance              // instance is deleted
+	done                         // terminal
 )
 
 type gc struct {
@@ -45,12 +45,12 @@ func TestSwarmEntities(t *testing.T) {
 
 	pollInterval := 100 * time.Millisecond
 
-	no_data := Tick(10)
-	docker_node_join := Tick(5)
-	wait_describe_instances := Tick(5)
-	wait_before_instance_destroy := Tick(3)
-	wait_before_reprovision := Tick(10) // wait before we reprovision a new instance to fix a Down node
-	wait_before_cleanup := Tick(10)
+	noData := Tick(10)
+	dockerNodeJoin := Tick(5)
+	waitDescribeInstances := Tick(5)
+	waitBeforeInstanceDestroy := Tick(3)
+	waitBeforeReprovision := Tick(10) // wait before we reprovision a new instance to fix a Down node
+	waitBeforeCleanup := Tick(10)
 
 	// actions
 	dockerNodeRm := &gc{
@@ -66,88 +66,88 @@ func TestSwarmEntities(t *testing.T) {
 	model, err := Define(
 		State{
 			Index: start,
-			TTL:   Expiry{no_data, timeout},
+			TTL:   Expiry{noData, timeout},
 			Transitions: map[Signal]Index{
-				docker_node_ready: matched_docker_node,
-				docker_node_down:  matched_docker_node,
-				instance_ok:       matched_instance,
-				timeout:           removed_instance, // nothing happened... cleanup
+				dockerNodeReady: matchedDockerNode,
+				dockerNodeDown:  matchedDockerNode,
+				instanceOK:      matchedInstance,
+				timeout:         removedInstance, // nothing happened... cleanup
 			},
 		},
 		State{
-			Index: matched_instance,
-			TTL:   Expiry{docker_node_join, docker_node_gone},
+			Index: matchedInstance,
+			TTL:   Expiry{dockerNodeJoin, dockerNodeGone},
 			Transitions: map[Signal]Index{
-				docker_node_ready: swarm_node,
-				docker_node_down:  swarm_node,
-				docker_node_gone:  pending_instance_destroy,
-				instance_gone:     removed_instance,
+				dockerNodeReady: swarmNode,
+				dockerNodeDown:  swarmNode,
+				dockerNodeGone:  pendingInstanceDestroy,
+				instanceGone:    removedInstance,
 			},
 			Actions: map[Signal]Action{
-				instance_gone: instanceDestroy.do,
+				instanceGone: instanceDestroy.do,
 			},
 		},
 		State{
-			Index: pending_instance_destroy,
-			TTL:   Expiry{wait_before_instance_destroy, reap},
+			Index: pendingInstanceDestroy,
+			TTL:   Expiry{waitBeforeInstanceDestroy, reap},
 			Transitions: map[Signal]Index{
-				docker_node_ready: swarm_node, // late joiner
-				docker_node_down:  swarm_node,
-				instance_gone:     removed_instance,
-				reap:              removed_instance,
+				dockerNodeReady: swarmNode, // late joiner
+				dockerNodeDown:  swarmNode,
+				instanceGone:    removedInstance,
+				reap:            removedInstance,
 			},
 			Actions: map[Signal]Action{
-				instance_gone: instanceDestroy.do,
-				reap:          instanceDestroy.do,
+				instanceGone: instanceDestroy.do,
+				reap:         instanceDestroy.do,
 			},
 		},
 		State{
-			Index: matched_docker_node,
-			TTL:   Expiry{wait_describe_instances, instance_gone},
+			Index: matchedDockerNode,
+			TTL:   Expiry{waitDescribeInstances, instanceGone},
 			Transitions: map[Signal]Index{
-				instance_ok:      swarm_node,
-				instance_gone:    removed_instance,
-				docker_node_gone: removed_instance, // could be docker rm'd out of band
+				instanceOK:     swarmNode,
+				instanceGone:   removedInstance,
+				dockerNodeGone: removedInstance, // could be docker rm'd out of band
 			},
 			Actions: map[Signal]Action{
-				instance_gone: dockerNodeRm.do,
+				instanceGone: dockerNodeRm.do,
 			},
 		},
 		State{
-			Index: swarm_node,
+			Index: swarmNode,
 			Transitions: map[Signal]Index{
-				docker_node_ready: swarm_node_ready,
-				docker_node_down:  swarm_node_down,
-				docker_node_gone:  matched_instance,
-				instance_gone:     matched_docker_node,
+				dockerNodeReady: swarmNodeReady,
+				dockerNodeDown:  swarmNodeDown,
+				dockerNodeGone:  matchedInstance,
+				instanceGone:    matchedDockerNode,
 			},
 		},
 		State{
-			Index: swarm_node_ready,
+			Index: swarmNodeReady,
 			Transitions: map[Signal]Index{
-				docker_node_down: swarm_node_down,
-				docker_node_gone: matched_instance,
-				instance_gone:    matched_docker_node,
+				dockerNodeDown: swarmNodeDown,
+				dockerNodeGone: matchedInstance,
+				instanceGone:   matchedDockerNode,
 			},
 		},
 		State{
-			Index: swarm_node_down,
-			TTL:   Expiry{wait_before_reprovision, docker_node_gone},
+			Index: swarmNodeDown,
+			TTL:   Expiry{waitBeforeReprovision, dockerNodeGone},
 			Transitions: map[Signal]Index{
-				docker_node_ready: swarm_node_ready,
-				docker_node_gone:  pending_instance_destroy,
-				instance_gone:     matched_docker_node,
+				dockerNodeReady: swarmNodeReady,
+				dockerNodeGone:  pendingInstanceDestroy,
+				instanceGone:    matchedDockerNode,
 			},
 		},
 		State{
-			Index: removed_instance, // after we removed the instance, we can still have unmatched node
-			TTL:   Expiry{wait_before_cleanup, timeout},
+			Index: removedInstance, // after we removed the instance, we can still have unmatched node
+			TTL:   Expiry{waitBeforeCleanup, timeout},
 			Transitions: map[Signal]Index{
-				docker_node_down: done,
-				timeout:          done,
+				dockerNodeDown: done,
+				timeout:        done,
 			},
 			Actions: map[Signal]Action{
-				docker_node_down: dockerNodeRm.do,
+				dockerNodeDown: dockerNodeRm.do,
 			},
 		},
 		State{
@@ -175,40 +175,40 @@ func TestSwarmEntities(t *testing.T) {
 		// case 1 - orphaned ex-leader node, a unmatched "Down" node where instance was removed and deletion observed.
 		// add new instance in start state
 		// we found the node via docker node ls and it's in ready state
-		require.NoError(t, group["case1"].Signal(docker_node_ready))
+		require.NoError(t, group["case1"].Signal(dockerNodeReady))
 
 		// we found the vm via plugin describe and it's in ok state
-		require.NoError(t, group["case1"].Signal(instance_ok))
+		require.NoError(t, group["case1"].Signal(instanceOK))
 
 		// another docker node ls, and it's still in ready
-		require.NoError(t, group["case1"].Signal(docker_node_ready))
+		require.NoError(t, group["case1"].Signal(dockerNodeReady))
 
 		// node does a swarm demote and leave -- docker node ls shows Down
-		require.NoError(t, group["case1"].Signal(docker_node_down))
+		require.NoError(t, group["case1"].Signal(dockerNodeDown))
 
 		// Down is unavailable
-		require.Equal(t, swarm_node_down, group["case1"].State())
+		require.Equal(t, swarmNodeDown, group["case1"].State())
 
 		// the vm is deleted -- diff of successive 'instance describe' calls show it's gone
-		require.NoError(t, group["case1"].Signal(instance_gone))
+		require.NoError(t, group["case1"].Signal(instanceGone))
 
 		// instance is gone but node is there
-		require.Equal(t, matched_docker_node, group["case1"].State())
+		require.Equal(t, matchedDockerNode, group["case1"].State())
 
 		// wait a bit longer and no news from future instance describes
-		time.Sleep(time.Duration(wait_describe_instances+1) * pollInterval)
+		time.Sleep(time.Duration(waitDescribeInstances+1) * pollInterval)
 
 		// should be deleted
-		require.Equal(t, removed_instance, group["case1"].State())
+		require.Equal(t, removedInstance, group["case1"].State())
 
 		// we should see the node getting removed.
 		gone := <-dockerNodeRm.ch
 
-		require.Equal(t, removed_instance, gone.State())
+		require.Equal(t, removedInstance, gone.State())
 		require.Equal(t, group["case1"].ID(), gone.ID())
 
 		// wait a bit and it will advance to done -- then we can clean up
-		time.Sleep(time.Duration(wait_before_cleanup+1) * pollInterval)
+		time.Sleep(time.Duration(waitBeforeCleanup+1) * pollInterval)
 
 		require.Equal(t, done, group["case1"].State())
 
@@ -224,29 +224,29 @@ func TestSwarmEntities(t *testing.T) {
 		group["case2"] = set.Add(start)
 
 		// we found the vm via plugin describe and it's in ok state
-		require.NoError(t, group["case2"].Signal(instance_ok))
+		require.NoError(t, group["case2"].Signal(instanceOK))
 
-		// there's a limit on docker_node_join timeout
-		time.Sleep(time.Duration(docker_node_join+1) * pollInterval)
+		// there's a limit on dockerNodeJoin timeout
+		time.Sleep(time.Duration(dockerNodeJoin+1) * pollInterval)
 
 		// waiting to be deleted... unless the swarm node shows up in time!
-		require.Equal(t, pending_instance_destroy, group["case2"].State())
+		require.Equal(t, pendingInstanceDestroy, group["case2"].State())
 
 		// we should see the node getting removed.
 		gone := <-instanceDestroy.ch
 
-		require.Equal(t, removed_instance, gone.State())
+		require.Equal(t, removedInstance, gone.State())
 		require.Equal(t, group["case2"].ID(), gone.ID())
 
 		// wait a bit and it will advance to done -- then we can clean up
-		time.Sleep(time.Duration(wait_before_cleanup+1) * pollInterval)
+		time.Sleep(time.Duration(waitBeforeCleanup+1) * pollInterval)
 
 		require.Equal(t, done, group["case2"].State())
 
 		// periodically clean up the deleted instances
 		set.Delete(group["case2"])
 		delete(group, "case2")
-		require.Equal(t, 0, set.CountByState(removed_instance))
+		require.Equal(t, 0, set.CountByState(removedInstance))
 		require.Equal(t, 0, set.Size())
 	}
 
@@ -257,53 +257,53 @@ func TestSwarmEntities(t *testing.T) {
 		group["case3"] = set.Add(start)
 
 		// we found the vm via plugin describe and it's in ok state
-		require.NoError(t, group["case3"].Signal(instance_ok))
+		require.NoError(t, group["case3"].Signal(instanceOK))
 
 		// we found the engine status via docker node ls and it's in Ready state
-		require.NoError(t, group["case3"].Signal(docker_node_ready))
+		require.NoError(t, group["case3"].Signal(dockerNodeReady))
 
 		// we are now matched
-		require.Equal(t, swarm_node, group["case3"].State())
+		require.Equal(t, swarmNode, group["case3"].State())
 
 		// docker node ls gives ready again
-		require.NoError(t, group["case3"].Signal(docker_node_ready))
+		require.NoError(t, group["case3"].Signal(dockerNodeReady))
 
 		time.Sleep(5 * pollInterval) // after a while
 
 		// a working ready swarm node
-		require.Equal(t, swarm_node_ready, group["case3"].State())
+		require.Equal(t, swarmNodeReady, group["case3"].State())
 
 		// the node goes offline -- engine disappeared / network partition
-		require.NoError(t, group["case3"].Signal(docker_node_down))
+		require.NoError(t, group["case3"].Signal(dockerNodeDown))
 
 		// now we are in a down state
-		require.Equal(t, swarm_node_down, group["case3"].State())
+		require.Equal(t, swarmNodeDown, group["case3"].State())
 
 		// we still see the matching instance
-		require.NoError(t, group["case3"].Signal(instance_ok))
+		require.NoError(t, group["case3"].Signal(instanceOK))
 
 		// we are still in the Down state
-		require.Equal(t, swarm_node_down, group["case3"].State())
+		require.Equal(t, swarmNodeDown, group["case3"].State())
 
 		// after some wait
-		time.Sleep(time.Duration(wait_before_reprovision+1) * pollInterval)
+		time.Sleep(time.Duration(waitBeforeReprovision+1) * pollInterval)
 
 		// the instance should be ready to be destroyed
-		require.Equal(t, pending_instance_destroy, group["case3"].State())
+		require.Equal(t, pendingInstanceDestroy, group["case3"].State())
 
 		// after a while, the engine still doesn't come back on within the limit...
-		time.Sleep(time.Duration(wait_before_instance_destroy+1) * pollInterval)
+		time.Sleep(time.Duration(waitBeforeInstanceDestroy+1) * pollInterval)
 
 		// we should see the instance getting removed.
 		gone := <-instanceDestroy.ch
 
-		require.Equal(t, removed_instance, gone.State())
+		require.Equal(t, removedInstance, gone.State())
 		require.Equal(t, group["case3"].ID(), gone.ID())
 
 		// at this point, the instance is gone... but we still have a docker node ls entry in Down state
 		// so another docker node ls shows this
 
-		require.NoError(t, group["case3"].Signal(docker_node_down))
+		require.NoError(t, group["case3"].Signal(dockerNodeDown))
 
 		// we should see the node getting removed.
 		gone2 := <-dockerNodeRm.ch
@@ -326,25 +326,25 @@ func TestSwarmEntities(t *testing.T) {
 		group["case4"] = set.Add(start)
 
 		// we found the engine status via docker node ls and it's in Ready state
-		require.NoError(t, group["case4"].Signal(docker_node_ready))
+		require.NoError(t, group["case4"].Signal(dockerNodeReady))
 
 		// now wait for an instance to show up from instance describe
-		require.Equal(t, matched_docker_node, group["case4"].State())
+		require.Equal(t, matchedDockerNode, group["case4"].State())
 
 		// after some wait
-		time.Sleep(time.Duration(wait_describe_instances+1) * pollInterval)
+		time.Sleep(time.Duration(waitDescribeInstances+1) * pollInterval)
 
-		require.Equal(t, removed_instance, group["case4"].State())
+		require.Equal(t, removedInstance, group["case4"].State())
 
 		// we should see the node get removed
 		gone := <-dockerNodeRm.ch
 
-		require.Equal(t, removed_instance, gone.State())
+		require.Equal(t, removedInstance, gone.State())
 		require.Equal(t, group["case4"].ID(), gone.ID())
 
 		// now the node is gone, but we don't have any more instance information because
 		// this instance wasn't adding by infrakit
-		time.Sleep(time.Duration(wait_before_cleanup+1) * pollInterval)
+		time.Sleep(time.Duration(waitBeforeCleanup+1) * pollInterval)
 
 		// final state
 		require.Equal(t, done, gone.State())
