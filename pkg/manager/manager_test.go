02cfc32e2d6de57feaf5548bbd13bc7d1de20995
Steven Kaufer <kaufer@us.ibm.com>
2018-02-08 14:40:26 -0600

    Issue second group commit on manager leadership change (#856)
    Signed-off-by: Steven Kaufer <kaufer@us.ibm.com>

--- pkg/manager/manager_test.go
+++ pkg/manager/manager_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"sync"
 	"testing"
 	"time"
 
@@ -260,7 +261,9 @@ func TestChangeLeadership(t *testing.T) {
 	checkpoint1 := make(chan struct{})
 	checkpoint2 := make(chan struct{})
 	checkpoint3 := make(chan struct{})
+	lock := sync.Mutex{}
 
+	commitCountMgr1 := 0
 	manager1, stoppable1 := testEnsemble(t, testDiscoveryDir(t), "m1", leaderChans[0], ctrl,
 		func(s *store_mock.MockSnapshot) {
 			empty := &[]entry{}
@@ -279,14 +282,19 @@ func TestChangeLeadership(t *testing.T) {
 		func(g *group_mock.MockPlugin) {
 			g.EXPECT().CommitGroup(gomock.Any(), false).Do(
 				func(spec group.Spec, pretend bool) (string, error) {
-
-					defer close(checkpoint1)
+					// Close the checkout channel after the 2nd commit
+					lock.Lock()
+					defer lock.Unlock()
+					commitCountMgr1++
+					if commitCountMgr1 == 2 {
+						defer close(checkpoint1)
+					}
 
 					require.Equal(t, gs.ID, spec.ID)
 					require.Equal(t, testToStruct(gs.Properties), testToStruct(spec.Properties))
 					return "ok", nil
 				},
-			).Return("ok", nil)
+			).Return("ok", nil).Times(2)
 
 			// We will get a call to inspect what's being watched
 			g.EXPECT().InspectGroups().Return([]group.Spec{gs}, nil)
@@ -301,6 +309,8 @@ func TestChangeLeadership(t *testing.T) {
 				},
 			).Return(nil)
 		})
+
+	commitCountMgr2 := 0
 	manager2, stoppable2 := testEnsemble(t, testDiscoveryDir(t), "m2", leaderChans[1], ctrl,
 		func(s *store_mock.MockSnapshot) {
 			empty := &[]entry{}
@@ -319,14 +329,19 @@ func TestChangeLeadership(t *testing.T) {
 		func(g *group_mock.MockPlugin) {
 			g.EXPECT().CommitGroup(gomock.Any(), false).Do(
 				func(spec group.Spec, pretend bool) (string, error) {
-
-					defer close(checkpoint2)
+					// Close the checkout channel after the 2nd commit
+					lock.Lock()
+					defer lock.Unlock()
+					commitCountMgr2++
+					if commitCountMgr2 == 2 {
+						defer close(checkpoint2)
+					}
 
 					require.Equal(t, gs.ID, spec.ID)
 					require.Equal(t, testToStruct(gs.Properties), testToStruct(spec.Properties))
 					return "ok", nil
 				},
-			).Return("ok", nil)
+			).Return("ok", nil).Times(2)
 		})
 
 	manager1.Start()
